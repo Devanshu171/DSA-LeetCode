1d80105ec070339cfd19faa1a586251042f8605a
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2021-10-11 13:09:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.02%) - LeetHub

--- combination-sum-iii/combination-sum-iii.cpp
+++ combination-sum-iii/combination-sum-iii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     
-    void helper(int ind,int n,int k, vector<int>&vec,vector<vector<int>>&ans)
+    void helper(int ind,int k,int n,vector<int>&vec,vector<vector<int>>&ans)
     {
-        if(vec.size()==n && k==0)
+        if(vec.size()==k)
         {
-            ans.push_back(vec);
-            return;
+            if(n==0)
+            {
+                ans.push_back(vec);
+                // return;
+            }
+            // return;
         }
-        if(k<0) return;
         
-        
-        for(int i=ind;i<10 && i<=k;i++ )
+        for(int i=ind;i<=9 && i<=n && vec.size()<=k;i++)
         {
             vec.push_back(i);
-            helper(i+1,n,k-i,vec,ans);
+            helper(i+1,k,n-i,vec,ans);
             vec.pop_back();
         }
-        
     }
-    vector<vector<int>> combinationSum3(int n, int k) {
-            vector<int>vec;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>vec;
         vector<vector<int>>ans;
-        helper(1,n,k,vec,ans);
+        helper(1,k,n,vec,ans);
         return ans;
     }
 };
