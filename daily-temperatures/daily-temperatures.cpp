5a091b3cdc801ecdf8d08e0e8b37035ca8510e3a
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2021-11-05 12:58:47 +0530

    Time: 152 ms (64.96%), Space: 84.9 MB (99.77%) - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n=temp.size();
         vector<int>vec(n);
-        stack<pair<int,int>>st;
+        stack<int>st;
         int k=n-1;
         for(int i=n-1;i>=0;i--){
 
-            while(!st.empty() && st.top().first<=temp[i]) st.pop();
+            while(!st.empty() && temp[st.top()]<=temp[i]) st.pop();
             
-            if(!st.empty()) vec[k--]=st.top().second-i;
+            if(!st.empty()) vec[k--]=st.top()-i;
             else vec[k--]=0;
             
-            st.push(make_pair(temp[i],i));
+            st.push(i);
         }
         return vec;
     }
