ade0681bdc3c17e01cffeb74f1d54523a3a39dbc
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2021-11-10 11:40:08 +0530

    Time: 20 ms (60.68%), Space: 7.7 MB (82.64%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cpp
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -1,57 +1,35 @@
-class Solution
-{
+class Solution {
 public:
-    string minWindow(string s, string t)
-    {
-           int i=0,j=0,MinL=INT_MAX,start=0;
-        unordered_map<char,int> mp;
+    string minWindow(string s, string t) {
+        unordered_map<char,int>mpp;
         for(auto it:t)
-            mp[it]++;
-        int count=mp.size();
-        
-        if(mp.find(s[j])!=mp.end()){
-            mp[s[j]]--;
-            if(mp[s[j]]==0)
-                count--;
-        }
-        
-        while(j<s.length()){
-            if(count>0){
-                j++;
-                if(mp.find(s[j])!=mp.end()){
-                    mp[s[j]]--;
-                    if(mp[s[j]]==0)
+            mpp[it]++;
+        int i=0,j=0,count=mpp.size(),minlen=INT_MAX,start;
+        while(j<s.size()){
+            if(mpp.find(s[j])!=mpp.end()){
+                mpp[s[j]]--;
+                if(mpp[s[j]]==0)
                     count--;
-                }
             }
+            if(count>0)
+                j++;
             else if(count==0){
-                // MinL=min(MinL,j-i+1);
-                if(MinL>j-i+1){
-                    MinL=j-i+1;
-                    start=i;
-                }
                 while(count==0){
-                    if(mp.find(s[i])!=mp.end()){
-                        mp[s[i]]++;
-                        if(mp[s[i]]==1){
-                            count++;
-                            // MinL=min(MinL,j-i+1);
-                             if(MinL>j-i+1){
-                                MinL=j-i+1;
-                                start=i;
-                            } 
-                        }
-                        i++;
-                    }
-                    else
-                        i++;
+                if(minlen>j-i+1){
+                    start=i;
+                    minlen=j-i+1;
                 }
+                if(mpp.find(s[i])!=mpp.end()){
+                        mpp[s[i]]++;
+                    if(mpp[s[i]]==1)
+                        count++;  
+                }
+                i++;
+                }
+                j++;
             }
+            
         }
-        string str="";
-        if(MinL!=INT_MAX)
-            return str.append(s.substr(start,MinL));
-        else
-            return str;
+        return minlen!=INT_MAX? s.substr(start,minlen):"";
     }
 };
