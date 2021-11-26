9bc761c520b935ccd4a6727fd654133feb120d83
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2021-11-26 17:07:50 +0530

    Time: 8 ms (79.52%), Space: 14.6 MB (8.93%) - LeetHub

--- flood-fill/flood-fill.cpp
+++ flood-fill/flood-fill.cpp
@@ -7,8 +7,9 @@ public:
                 vector<vector<int>> colored(n,vector<int>(m,0));
 
      int oc= image[sr][sc];
+        if(oc==newColor) return image;
             image[sr][sc]=newColor;
-        colored[sr][sc]=1;
+    
         
         q.push({sr,sc});        
         int dx[]={-1,0,0,1};
@@ -23,9 +24,9 @@ public:
             for(int i=0;i<4;i++){
                 int newX=x+dx[i];
                 int newY=y+dy[i];
-                if(newX>=0 && newX<n && newY>=0 && newY<m &&image[newX][newY]==oc && colored[newX][newY]!=1){
+                if(newX>=0 && newX<n && newY>=0 && newY<m &&image[newX][newY]==oc){
                     image[newX][newY]=newColor;
-                    colored[newX][newY]=1;
+                    
                     q.push({newX,newY});
                 }
             }
