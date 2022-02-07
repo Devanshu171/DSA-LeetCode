532c12127871cf37128af66292ecc91d400a6d35
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-02-07 21:49:13 +0530

    Time: 11 ms (13.53%), Space: 11.2 MB (29.40%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    int pivot(vector<int>&nums,int n){
+    int pivot(vector<int>&arr,int n){
+        
         int start=0,end=n-1;
-        if(nums[0]<=nums[n-1])
-                return 0;
+        
+        if(arr[0]<=arr[n-1]) return 0;
+    
         while(start<=end){
-            int mid=start+(end-start)/2;
             
-            int next=nums[(mid+1)%n],prev=nums[(mid-1+n)%n];
-            if(nums[mid]<=next && nums[mid]<=prev)
-                return mid;
-            else{
-                if(nums[mid]<=nums[end])
-                    end=mid-1;
-                else if(nums[mid]>=nums[start]){
-                    start=mid+1;
-                }
-            }
-           
-            
-        }
-         return -1;
-    }
-    int bs(vector<int>&nums,int start,int end,int x){
-                
-        // int start=0,end=n-1;
-        while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==x)
+            int next=arr[(mid+1)%n],prev=arr[(mid-1+n)%n];
+            
+            if(arr[mid]<=next && arr[mid]<=prev)
                 return mid;
-            else if(nums[mid]>x)
+             else if(arr[mid] <= arr[end])  
                 end=mid-1;
-            else
+            else if(arr[mid] >=arr[start]) 
                 start=mid+1;
+           
+        }
+        return -1;
+                      
+    }
+    
+    int bs(vector<int>&arr,int start,int end,int x){
+        int n=arr.size();
+        while(start<=end){
+            
+            int mid=start+(end-start)/2;
+            if(arr[mid]==x) return mid;
+            else if(arr[mid]>x) end=mid-1;
+            else start=mid+1;
+            
         }
         return -1;
     }
-    int search(vector<int>& nums, int x) {
-        int n=nums.size();
-        int pvt=pivot(nums,n);
-        int ans1=bs(nums,0,pvt-1,x);
-        if(ans1!=-1)
-            return ans1;
-        else{
-            return bs(nums,pvt,n-1,x);
-        }
+    int search(vector<int>& arr, int x) {
+        int n=arr.size();
+        int pvt=pivot(arr,n);
+         cout<<pvt;
+        int ans=bs(arr,0,pvt-1,x) ;
+       
+        if(ans!=-1)return ans;
+            else return  bs(arr,pvt,n-1,x); ;
+        
+        
     }
 };
