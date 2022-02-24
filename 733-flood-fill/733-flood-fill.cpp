1d8224fb76ba0f057f116550de26df86366b96c6
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-02-24 20:45:26 +0530

    Time: 16 ms (38.13%), Space: 15.7 MB (6.82%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    bool isValid(int i,int j,int n,int m,int color,vector<vector<int>>image,vector<vector<int>>vis){
-        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==color && vis[i][j]==0) return true;
+    bool isValid(int i,int j,int n,int m,int color,vector<vector<int>>image){
+        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==color) return true;
         return false;
+    }
+    void fill(int i,int j,int n,int m,int newColor,int oldcolor,vector<vector<int>>&image){
+        image[i][j]=newColor;
+        int di[]={1,-1,0,0};
+        int dj[]={0,0,-1,1};
+        for(int k=0;k<4;k++){
+            if(isValid(i+di[k],j+dj[k],n,m,oldcolor,image)){
+                fill(i+di[k],j+dj[k],n,m,newColor,oldcolor,image);
+            }
+        }
+        
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int n=image.size();
         int m=image[0].size();
-        queue<pair<int,int>>q;
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        
-        q.push({sr,sc});
-        vis[sr][sc]=1;
-        
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
-            int curColor=image[i][j];
-            image[i][j]=newColor;
-            int di[]={1,-1,0,0};
-            int dj[]={0,0,1,-1};
-            for(int k=0;k<4;k++){
-                if(isValid(i+di[k],j+dj[k],n,m,curColor,image,vis)){
-                    vis[i+di[k]][j+dj[k]]=1;
-                    q.push({i+di[k],j+dj[k]});
-                }
-            }
-        }
+        if(image[sr][sc]!=newColor)
+        fill(sr,sc,n,m,newColor,image[sr][sc],image);
         return image;
     }
 };
