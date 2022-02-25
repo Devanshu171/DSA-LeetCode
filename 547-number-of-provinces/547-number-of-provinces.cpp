709bc9f96a8684e09683a85aaa8be8b85598da1d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-02-26 00:14:01 +0530

    Time: 16 ms (99.04%), Space: 14.4 MB (20.45%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,51 +1,31 @@
-class DSU{
-    private: vector<int>parent,size;
-    int count=0;
-    public: DSU(int n){
-        count=n;
-        for(int i=0;i<n;i++){
-            parent.push_back(i);
-            size.push_back(1);
-        }
-    }
-    public: int findParent(int node){
-        if(parent[node]==node) return node;
-        return parent[node]=findParent(parent[node]);
-    }
-   public: void Union(int u,int v){
-        int up=findParent(u);
-        int vp=findParent(v);
-        
-        if(up==vp) return ;
-        else if(size[up]>size[vp]){
-            parent[vp]=up;
-            size[up]+=size[vp];
-        }else{
-             parent[up]=vp;
-            size[vp]+=size[up];
-        }
-        count--;
-    }
-   public: int ans(){
-        return count;
-    }
-};
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& arr) {
-        int count=0;
-        int n=arr.size();
-        DSU dsu(n);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                cout<<i<<" "<<j<<endl;
-                cout<<arr[i][j]<<" "<<endl;
-            if(arr[i][j]==1){
-              dsu.Union(i,j);
-               }
-            }       
+    void dfs(int node,vector<int>&vis,vector<int>adj[]){
+        
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,vis,adj);
+            }
         }
-        return dsu.ans();
-        return 0;
+    }
+    int findCircleNum(vector<vector<int>>& a) {
+        int n=a.size();
+        vector<int>adj[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(a[i][j]==1)
+            adj[i].push_back(j);
+            }
+        }
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                count++;
+                dfs(i,vis,adj);
+            }
+        }
+        return count;
     }
 };
