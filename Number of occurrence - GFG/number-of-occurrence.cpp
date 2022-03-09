88899e9627877621e03f6b3c28d386127acc8a30
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-09 23:34:59 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -12,48 +12,17 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-		int first(int arr[],int n,int x){
-		    int start=0,end=n-1,ans=-1;
-		    while(start<=end){
-		        int mid=start+(end-start)/2;
-		        if(arr[mid]==x){
-		            ans=mid;
-		            end=mid-1;
-		        }
-		        else if(arr[mid]>x){
-		            
-		            end=mid-1;
-		        }else{
-		            start=mid+1;
-		        }
-		    }
-		    return ans;
-		}
-		int last(int arr[],int n,int x){
-		    int start=0,end=n-1,ans=-1;
-		    while(start<=end){
-		        int mid=start+(end-start)/2;
-		        if(arr[mid]==x){
-		            ans=mid;
-		            start=mid+1;
-		        }
-		        else if(arr[mid]>x){
-		            
-		            end=mid-1;
-		        }else{
-		            start=mid+1;
-		        }
-		    }
-		    return ans;
-		}
-	int count(int arr[], int n, int x) {
-	    // code here
-	    int frst=first(arr,n,x);
-	    int lst=last(arr,n,x);
-	    if(frst!=-1)
-	    return lst-frst+1;
-	    else return 0;
-	}
+int count(int arr[], int n, int x) {
+    // code here
+    int count =0;
+    for(int i=0; i<n; i++){
+        if(arr[i]==x){
+            count++;
+        }
+    }
+    return count;
+}
+
 };
 
 // { Driver Code Starts.
