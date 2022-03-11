98ed88ecf5c047f2ef718190719f5fff982556dd
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-11 19:17:13 +0530

    Time: 8 ms (41.08%), Space: 12 MB (75.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -5,31 +5,34 @@ public:
         a=b;
         b=temp;
     }
-    void reverse(vector<int>&nums,int start,int end){
-        while(start<end){
-            swap(nums[start],nums[end]);
-            start++;
-            end--;
+    void reverse(vector<int>&nums,int s,int e){
+        while(s<e){
+            swap(nums[s],nums[e]);
+            s++;
+            e--;
         }
     }
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return;
         int indx1=-1,indx2;
+        if(n<=1) return;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 indx1=i;
                 break;
             }
         }
-       if(indx1!=-1){ for(int i=n-1;i>=0;i--){
+        if(indx1!=-1){
+            for(int i=n-1;i>=0;i--){
                 if(nums[i]>nums[indx1]){
                     indx2=i;
                     break;
                 }
+            }
+            swap(nums[indx1],nums[indx2]);
         }
-        
-        swap(nums[indx1],nums[indx2]);}
         reverse(nums,indx1+1,n-1);
+            // return nums;
+        
     }
 };
