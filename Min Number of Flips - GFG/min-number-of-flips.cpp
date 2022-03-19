e83d2ff92df524336e7926cd084f5884fc9e5d5d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-19 16:10:01 +0530

    Added solution - LeetHub

--- Min Number of Flips - GFG/min-number-of-flips.cpp
+++ Min Number of Flips - GFG/min-number-of-flips.cpp
@@ -23,20 +23,11 @@ int minFlips (string s)
     int c1=0,c0=0,n=s.size();
     for(int i=0;i<n;i++){
         if(i&1){
-            if(s[i]=='1'){
-                c1++;
-                
-            }
-            else{
-                c0++;
-            }
+            if(s[i]=='1') c1++;
+            else c0++;
         }else{
-            if(s[i]=='1'){
-                c0++;
-            }else{
-                c1++;
-                
-            }
+            if(s[i]=='1')c0++;
+            else c1++;
         }
     }
     return min(c1,c0);
