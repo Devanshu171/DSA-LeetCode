53841d6594c4a49d2dd600ed79fec6e65736dc43
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-20 15:11:33 +0530

    Added solution - LeetHub

--- Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.cpp
+++ Minimum Swaps for Bracket Balancing - GFG/minimum-swaps-for-bracket-balancing.cpp
@@ -6,26 +6,20 @@ using namespace std;
 class Solution{   
 public:
     int minimumNumberOfSwaps(string s){
-        // code here 
-        vector<int>pos;
+
         int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]=='[')
-            pos.push_back(i);
-        }
-        int p=0,count=0,swaps=0;
+       
+        int open=0,close=0,swaps=0,unbalanced=0;
         for(int i=0;i<n;i++){
             if(s[i]=='['){
-                count++;
-                p++;
-            }else{
-                count--;
-                if(count<0){
-                    swaps+=pos[p]-i;
-                    swap(s[i],s[pos[p]]);
-                    p++;
-                    count=1;
+                open++;
+                if(unbalanced>0){
+                    swaps+=unbalanced;
+                    unbalanced--;
                 }
+            }else{
+                close++;
+                unbalanced=close-open;
             }
         }
         return swaps;
