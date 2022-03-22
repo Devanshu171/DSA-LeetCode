a5fc1ad206377474d83f97acf907a061db5f43eb
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-22 18:59:24 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -7,33 +7,36 @@ using namespace std;
 class Solution
 {
     public:    
-     vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
+       vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
         {
             //code here.
-           int i=0,j=0,k=0;
-           vector<int>v;
-           while(i<n1 && j<n2 && k<n3){
-               if(a[i]==b[j] && b[j]==c[k]){
-                   v.push_back(a[i]);
-                   i++;j++;k++;
-               }
-               else if(a[i]<b[j]) i++;
-               else if(b[j]<c[k]) j++;
-               else k++;
-               int xx=a[i-1];
-               while(a[i]==xx) i++;
-               int yy=b[j-1];
-               while(b[j]==yy) j++;
-               int zz=c[k-1];
-               while(c[k]==zz) k++;
-           }
-           if(v.size()==0) return{ -1};
-            else return v;
-
-            
+            int i=0,j=0,k=0,p1=INT_MIN,p2=INT_MIN,p3=INT_MIN;
+            vector<int>ans;
+            while(i<n1 && j<n2 && k <n3){
+                
+                while(i<n1 && a[i]==p1) i++;
+                while(j<n1 && b[j]==p2) j++;
+                while(k<n3 && c[k]==p3) k++;
+                
+                if(a[i]==b[j] && b[j]==c[k]){
+                    ans.push_back(a[i]);
+                    p1=a[i],p2=b[j],p3=c[k];
+                    i++;j++;k++;
+                }else if(a[i]<b[j]){
+                    p1=a[i];
+                    i++;
+                }else if(b[j]<c[k]){
+                    p2=b[j];
+                    j++;
+                }else{
+                    p3=c[k];
+                    k++;
+                }
+                    
+            }
+            return ans;
             
         }
-
 
 };
 
