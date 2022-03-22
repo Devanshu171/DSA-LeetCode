e0ac7120c107671600cc131641146014802757c0
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-22 15:39:08 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -11,13 +11,45 @@ class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
-        //code here
-        unordered_set<int>st;
-        for(int i=0;i<n;i++) st.insert(a[i]);
-        for(int i=0;i<m;i++) st.insert(b[i]);
-        return st.size();
         
-    }
+        int i=0,j=0,ans=0;
+        sort(a,a+n);
+        sort(b,b+m);
+        while(i<n && j<m){
+            while(i<n-1 && a[i]==a[i+1]) i++;
+            while(j<m-1 && b[j]==b[j+1]) j++;
+            if(a[i]<b[j]){
+                ans++;
+                i++;
+            }else if(a[i]>b[j]){
+                ans++;
+                j++;
+            }else{
+                ans++;
+                i++;
+                j++;
+            }
+            
+           }
+           while(i<n) {
+               if(i<n-1 && a[i]==a[i+1]){
+               i++;
+               }else{
+               i++;ans++;
+               }
+           }
+           while(j<m){
+               if(j,m-1 && b[j]==b[j+1])
+               j++;
+               else{
+               j++;ans++;
+               }
+           } 
+           return ans;
+            
+        }
+        
+    
 };
 
 // { Driver Code Starts.
