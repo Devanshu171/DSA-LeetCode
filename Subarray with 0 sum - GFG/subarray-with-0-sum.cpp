e2e00c4b2ad02f396212beb8a796d058e4cbf2b0
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-22 19:19:21 +0530

    Added solution - LeetHub

--- Subarray with 0 sum - GFG/subarray-with-0-sum.cpp
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.cpp
@@ -13,11 +13,12 @@ class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     bool subArrayExists(int arr[], int n)
     {
+        //Your code here
         unordered_set<int>st;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum==0 ||st.find(sum)!=st.end()) return true;
+            if(sum==0 || st.count(sum)>0) return true;
             st.insert(sum);
         }
         return false;
