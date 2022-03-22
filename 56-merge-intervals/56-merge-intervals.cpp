92113452feae601bff1d0822121e1f0e1f6902fd
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-22 17:26:21 +0530

    Time: 32 ms (96.33%), Space: 19.2 MB (44.47%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,13 +3,12 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>ans;
         int n=intervals.size();
-        if(n<=1) return intervals;
         sort(intervals.begin(),intervals.end(),[](vector<int>&a,vector<int>&b){
             return a[0]<b[0];
         });
         vector<int>cur=intervals[0];
         for(int i=1;i<n;i++){
-            if(cur[1]>=intervals[i][0]){
+            if(intervals[i][0]<=cur[1]){
                 cur[1]=max(cur[1],intervals[i][1]);
             }else{
                 ans.push_back(cur);
