d01ee123fe237127fe3960a4ad3d16467c6d2a5d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-23 14:43:27 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.c
+++ Palindrome String - GFG/palindrome-string.c
@@ -14,7 +14,7 @@ int isPalindrome(char S[])
     // int n=sizeof(S)/sizeof(S[i]);
     int len=strlen(S);
     
-    for(int i=0;i<len;i++){
+    for(int i=0;i<len/2;i++){
         if(S[i]!=S[len-i-1])
         return 0;
     }
