b721acb234a590dbec0a2d75a04e98feb935ab68
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-24 14:56:39 +0530

    Added solution - LeetHub

--- Count the Reversals - GFG/count-the-reversals.cpp
+++ Count the Reversals - GFG/count-the-reversals.cpp
@@ -20,16 +20,19 @@ int main()
 int countRev (string s)
 {
     // your code here
-    stack<char>st;
-    int n=s.size();
-    if(n&1) return -1;
     int op=0,cl=0;
-        for(int i=0;i<n;i++){
-           if(s[i]=='{') op++;
-           else{
-               if(op==0) cl++;
-               else op--;
-           }
+    if(s.size()&1) return -1;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='{'){
+            op++;
+        }else{
+            if(op==0){
+                cl++;
+            }else{
+                op--;
+            }
         }
-    return (cl+1)/2+(op+1)/2;
+    }
+    if((op+cl)&1) return -1;
+    return (op+1)/2+(cl+1)/2;
 }
