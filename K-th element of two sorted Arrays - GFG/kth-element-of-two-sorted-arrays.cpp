fd991a929c20212f3920f807f0bf89cda350fcd5
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-30 18:24:50 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,51 +8,30 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int i=0,j=0;
-        if(k>m+n-1)
-        return -1;
-        int count=1;
-        while(i<n && j<m){
-            if(arr1[i]<arr2[j]){
-                if(count==k){
-                return arr1[i];
-                break;
-                }else{
-                    count++;
-                    i++;
-                }
-                
-            }else{
-                if(count==k){
-                    return arr2[j];
-                    break;
-                }else{
-                    j++;
-                    count++;
-                }
-            }
-        }
-        while(i<n){
-            if(count==k){
-                return arr1[i];
-                break;
-            }else{
-                count++;
-                i++;
-            }
-        }
-        while(j<m){
-            if(count==k){
-                return arr2[j];
-                break;
-            }else{
-                count++;
-                j++;
-            }
-        }
-        return 0;
+        if(n>m) return kthElement(arr2,arr1,m,n,k);
+        int low=max(0,k-m),high=min(k,n);
         
+        while(low<=high){
+            int cut1=low+(high-low)/2;
+            int cut2=k-cut1;
+            
+            int left1=cut1==0?INT_MIN:arr1[cut1-1];
+            int left2=cut2==0?INT_MIN:arr2[cut2-1];
+            
+            int right1=cut1==n?INT_MAX:arr1[cut1];
+            int right2=cut2==m?INT_MAX:arr2[cut2];
+            
+            if(left1<=right2 && left2<=right1){
+                return max(left1,left2);
+            }else if(left1>right2){
+                high=cut1-1;
+        }else{
+            low=cut1+1;
+        }
+        }
+         return -1;
     }
+   
 };
 
 // { Driver Code Starts.
