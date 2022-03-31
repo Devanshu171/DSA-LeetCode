3ace19b49d106e6474d561946b6f887d141d4dc5
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-03-31 15:05:50 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -12,44 +12,36 @@ class Solution
 {
     public:
     //Function to find minimum number of pages.
-    bool isValid(int a[],int n,int k,int mx){
-        int student=1;
-        int sum=0;
+    bool ispossible(int arr[],int n,int m,int mx){
+        int students=1,sum=0;
         for(int i=0;i<n;i++){
-          
-            
-            if(sum+a[i]>mx){
-                student++;
-                sum=a[i];
+            sum+=arr[i];
+            if(sum>mx){
+                students++;
+                sum=arr[i];
             }
-            else sum+=a[i];
-           
         }
-        return student<=k;
+        return students<=m;
     }
-    int findPages(int a[], int n, int k) 
+    int findPages(int arr[], int n, int m) 
     {
         //code here
-        if(n<k) return -1;
-        int sum=0,mx=INT_MIN;
+        int low=-1,high=0,ans=-1;
         for(int i=0;i<n;i++){
-            sum+=a[i];
-            mx=max(mx,a[i]);
+            high+=arr[i];
+            low=max(low,arr[i]);
         }
-        int start=mx,end=sum;
         
-        int res=-1;
-       while(start<=end){
-            int mid=start+(end-start)/2;
-            if(isValid(a,n,k,mid)){
-                res=mid;
-                end=mid-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(ispossible(arr,n,m,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
-            else{
-                start=mid+1;
-            }   
         }
-        return res;
+        return ans;
     }
 };
 
