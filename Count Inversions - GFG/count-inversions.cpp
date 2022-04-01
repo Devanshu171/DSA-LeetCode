4fd71b1a2f24a6497bddd432503b2b992542f772
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-01 18:32:36 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -4,46 +4,46 @@ using namespace std;
 
 
  // } Driver Code Ends
-
-#define ll long long
 class Solution{
   public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
+    #define ll long long
     ll merge(ll arr[],ll low,ll mid,ll high){
-        ll i=low,j=mid,k=0;
+        ll i=low,j=mid;
         ll temp[high-low+1];
+        ll k=0;
         ll ans=0;
         while(i<mid && j<=high){
-            if(arr[i]>arr[j]){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }else{
                 ans+=mid-i;
                 temp[k++]=arr[j++];
-            }else{
-                temp[k++]=arr[i++];
             }
         }
         while(i<mid) temp[k++]=arr[i++];
         while(j<=high) temp[k++]=arr[j++];
         k=0;
-        for(int i=low;i<=high;i++){
-            arr[i]=temp[k++];
-        }
-        return ans;     
+        for(i=low;i<=high;i++)
+        arr[i]=temp[k++];
+        
+        return ans;
     }
     ll mergesort(ll arr[],ll low,ll high){
-        ll total=0;
+        ll ans=0;
         if(low<high){
             ll mid=low+(high-low)/2;
-            total+=mergesort(arr,low,mid);
-            total+=mergesort(arr,mid+1,high);
-            total+=merge(arr,low,mid+1,high);
+            ans+=mergesort(arr,low,mid);
+            ans+=mergesort(arr,mid+1,high);
+            ans+=merge(arr,low,mid+1,high);
         }
-        return total;
+        return ans;
     }
-    long long int inversionCount(long long arr[], long long N)
+    long long int inversionCount(long long arr[], long long n)
     {
-        return mergesort(arr,0,N-1);
+        return mergesort(arr,0,n-1);
     }
 
 };
