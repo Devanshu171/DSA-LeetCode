461c6276006a6b726527e979249b3183065df2c3
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-03 15:42:32 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -9,33 +9,25 @@ class Solution
     public:    
        vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
         {
-            //code here.
-            int i=0,j=0,k=0,p1=INT_MIN,p2=INT_MIN,p3=INT_MIN;
             vector<int>ans;
-            while(i<n1 && j<n2 && k <n3){
-                
-                while(i<n1 && a[i]==p1) i++;
-                while(j<n1 && b[j]==p2) j++;
-                while(k<n3 && c[k]==p3) k++;
-                
+            int i=0,j=0,k=0;
+            while(i<n1 && j<n2 && k<n3 ){
                 if(a[i]==b[j] && b[j]==c[k]){
-                    ans.push_back(a[i]);
-                    p1=a[i],p2=b[j],p3=c[k];
+                    if(!ans.empty() && ans.back()==a[i]) {
+                    i++;j++;k++;
+                        continue;
+                    }
+                    ans.push_back(b[j]);
                     i++;j++;k++;
                 }else if(a[i]<b[j]){
-                    p1=a[i];
                     i++;
                 }else if(b[j]<c[k]){
-                    p2=b[j];
                     j++;
                 }else{
-                    p3=c[k];
                     k++;
                 }
-                    
             }
             return ans;
-            
         }
 
 };
