a3a68ac4b609a9d4acd576f5bbf904080a7e6fb1
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-05 13:50:54 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -10,30 +10,29 @@ using namespace std;
 
 class Solution {
   public:
-  
     int romanToDecimal(string &str) {
         // code here
         unordered_map<char,int>mpp;
-               mpp['V']= 5;
-                mpp['I']= 1; 
-                mpp['X']= 10;
-                mpp['L']= 50;
-                mpp['C']= 100;
-                 mpp['D']= 500;
-                 mpp['M']= 1000;
-               int n=str.size();
-        int ans=0;
-        int prev=0;
-        for(int i=n-1;i>=0;i--){
-           if(mpp[str[i]]<prev){
-               ans-=mpp[str[i]];
-           }else{
-               ans+=mpp[str[i]];
-           }
-           prev=mpp[str[i]];
-        //   cout<<prev<<" ";
-        }
-        return ans;
+                 mpp['I']=1;
+                mpp['V']= 5;
+                mpp['X']=10;
+                mpp['L']=50;
+                mpp['C'] =100;
+                mpp['D']=500;
+                mpp['M']=1000;
+                
+                int ans=0,prev=0;
+                
+                for(int i=str.size()-1;i>=0;i--){
+                    if(mpp[str[i]]>=prev){
+                        ans+=mpp[str[i]];
+                        prev=mpp[str[i]];
+                    }else{
+                        ans-=mpp[str[i]];
+                        prev=mpp[str[i]];
+                    }
+                }
+                return ans;
     }
 };
 
