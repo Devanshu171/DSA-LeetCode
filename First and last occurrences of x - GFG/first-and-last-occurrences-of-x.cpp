4421643294a5f3e32e181acb45ce1c7a4afca5ff
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-06 14:11:56 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -4,32 +4,35 @@ using namespace std;
 
  // } Driver Code Ends
 
-int bs(int arr[],int n,int x,int dir){
-    int start=0,end=n-1,ans=-1;
-    while(start<=end){
-        int mid=start+(end-start)/2;
+int bs(int arr[],int n,int x,int flag){
+    int s=0,e=n-1,ans=-1;
+    while(s<=e){
+        int mid=s+(e-s)/2;
         if(arr[mid]==x){
             ans=mid;
-            if(dir==0){
-                end=mid-1;
+            if(flag){
+                s=mid+1;
             }else{
-                start=mid+1;
+                e=mid-1;
             }
-        }else if(arr[mid]<x){
-           start=mid+1;
+        }else if(arr[mid]>x){
+            e=mid-1;
         }else{
-            end=mid-1;
+            s=mid+1;
         }
     }
     return ans;
 }
 
 vector<int> find(int arr[], int n , int x )
+
+
 {
-    // code here
     int first=bs(arr,n,x,0);
-    int last=bs(arr,n,x,1);
-    return {first,last};
+    if(!first==-1) return {-1};
+    
+int last= bs(arr,n,x,1);
+return {first,last};
 }
 
 // { Driver Code Starts.
