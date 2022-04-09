52dfcef164dc18d653b3cbf09c718d8768c0c58f
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-09 23:54:55 +0530

    Time: 5 ms (30.21%), Space: 7.1 MB (64.77%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-  void helper(int ind,vector<int>&arr,vector<int>&temp,vector<vector<int>>&ans,int n){
-                    
-        if(ind==n){
-                ans.push_back(temp);
-             return;
-            }
-                
-        temp.push_back(arr[ind]);
-        helper(ind+1,arr,temp,ans,n);
-        temp.pop_back();
-         helper(ind+1,arr,temp,ans,n);
-    }
+
     vector<vector<int>> subsets(vector<int>& arr) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        helper(0,arr,temp,ans,arr.size());
-        return ans;
+        int n = arr.size();
+  int subset_ct = (1 << n); // 2^n
+  vector<vector<int>> ans;
+  for (int mask = 0; mask < subset_ct; mask++)
+  {
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+      if ((mask & (1 << i)) != 0) // (mask & (1<<i))!=0
+        temp.push_back(arr[i]);
+    }
+    ans.push_back(temp);
+  }
+  return ans;
     }
 };
