67e12b7f558a24c6177953bc611caa17ebc2c094
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-12 14:58:50 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (14.49%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-    vector<int> getlps(string s){
-        vector<int>ans(s.size());
-        ans[0]=0;
-        int i=1,j=0;
-        while(i<s.size()){
-            if(s[i]==s[j]){
-                ans[i]=j+1;
-                i++;j++;
-            }else{
-                if(j!=0){
-                    j=ans[j-1];
-                }else{
-                    ans[i]=0;
-                    i++;
-                }
-            }
-        }
-        return ans;
-        
-    }
+
     bool rotateString(string s, string goal) {
       if(s.size()!=goal.size()) return false;
-        s=s+s;
-        vector<int>lps(goal.size());
-        lps=getlps(s);
-        int j=0,i=0;
-        while(i<s.size() && j<goal.size()){
-            if(s[i]==goal[j]){
-                i++;j++;
-            }else{
-                if(j!=0)
-                j=lps[j-1];
-                else{
-                    i++;
-                }
+        for(int i=0;i<s.size();i++){
+            if(rotate(s,goal,i)){
+                return true;
             }
         }
-        return j==goal.size();
-        
+        return false;      
+    }
+    bool rotate(string s,string goal,int k){
+        for(int i=0;i<s.size();i++){
+            if(goal[i]!=s[(i+k)%s.size()])
+                return false;
+        }
+        return true;
     }
 };
