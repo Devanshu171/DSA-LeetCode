7863077a293b05e205c162bc09bfea32984d3fd3
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-23 15:42:24 +0530

    Time: 4 ms (70.24%), Space: 11.2 MB (29.82%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,55 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int s=0,e=n-1;
+    int pivot(vector<int>&arr){
+        int s=0,e=arr.size()-1;
+        int n=arr.size();
+        // while(s<e){
+        //     int mid=s+(e-s)/2;
+        //     if(arr[mid]>=arr[0])
+        //         s=mid+1;
+        //     else
+        //         e=mid;
+        // }
+        // return s;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]==target)
+           
+            int left=arr[(mid-1+n)%n];
+            int right=arr[(mid+1)%n];
+         cout<<mid<<endl;
+            if(arr[mid]>=right)
+                return (mid+1)%n;
+            else if(arr[mid]<=left)
                 return mid;
-            
-         else  if(nums[s]<=nums[mid]){
-                if(nums[s]<=target && target<=nums[mid]){
-                    e=mid-1;
-                }
-               else{
-                   s=mid+1;
-               }
+            else if(arr[mid]>=arr[s]){
+                s=mid+1;
             }else{
-               if(nums[mid]<=target && target<=nums[e]){
-                   s=mid+1;
-               }else{
-                   e=mid-1;
-               }
-           }
+                e=mid-1;
+            }
         }
         return -1;
+    }
+    int bs(vector<int>&arr,int s,int e,int x){
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]==x)
+                return mid;
+            else if(arr[mid]>x)
+                e=mid-1;
+            else
+                s=mid+1;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int pvt=pivot(nums);
+        cout<<pvt<<endl;
+        int ans1=bs(nums,0,pvt-1,target);
+        if(ans1!=-1)
+            return ans1;
+        else 
+            return bs(nums,pvt,nums.size()-1,target);
+        
     }
 };
