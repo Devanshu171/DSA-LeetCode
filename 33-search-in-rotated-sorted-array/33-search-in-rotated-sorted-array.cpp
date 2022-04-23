5f5e3535db27838a10c81520afe00ec5a9d25c0f
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-23 15:06:48 +0530

    Time: 3 ms (85.29%), Space: 11.1 MB (75.94%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int pivot(vector<int>&arr){
-        int s=0,e=arr.size()-1;
-        while(s<e){
-            int mid=s+(e-s)/2;
-            if(arr[mid]>=arr[0])
-                s=mid+1;
-            else
-                e=mid;
-        }
-        return s;
-    }
-    int bs(vector<int>&arr,int s,int e,int x){
-        
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int s=0,e=n-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(arr[mid]==x)
+            if(nums[mid]==target)
                 return mid;
-            else if(arr[mid]>x)
-                e=mid-1;
-            else
-                s=mid+1;
+            
+         else  if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && target<=nums[mid]){
+                    e=mid-1;
+                }
+               else{
+                   s=mid+1;
+               }
+            }else{
+               if(nums[mid]<=target && target<=nums[e]){
+                   s=mid+1;
+               }else{
+                   e=mid-1;
+               }
+           }
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int pvt=pivot(nums);
-        cout<<pvt<<endl;
-        int ans1=bs(nums,0,pvt-1,target);
-        if(ans1!=-1)
-            return ans1;
-        else 
-            return bs(nums,pvt,nums.size()-1,target);
-        
     }
 };
