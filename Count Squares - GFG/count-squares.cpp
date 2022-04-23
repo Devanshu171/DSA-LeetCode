4c6dbeca8f8007c658553d47bde4db597b9bde35
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-23 15:56:11 +0530

    Added solution - LeetHub

--- Count Squares - GFG/count-squares.cpp
+++ Count Squares - GFG/count-squares.cpp
@@ -5,24 +5,23 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-  #define ll long long
-    int countSquares(int n) {
+    int countSquares(int N) {
         // code here
-      int s=1,e=n;
-      int ans=-1;
-      while(s<=e){
-          int mid=s+(e-s)/2;
-          if( mid*(ll)mid<=n){
-              ans=mid;
-              s=mid+1;
-          }else{
-              e=mid-1;
-          }
-      }
-      if(ans*(ll)ans!=n)
-      return ans;
-      else return ans-1;
-     
+        int s=0,e=N,ans;
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            long long ele=mid*1LL*mid;
+            // cout<<ele<<endl;
+            if(ele<=N){
+                ans=mid;
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+            
+        }
+        return ans*ans==N? ans-1:ans;
     }
 };
 
