aa4b3d37b69a1e5fa3b0fc107d914dc30a402c9c
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-24 17:48:19 +0530

    Added solution - LeetHub

--- Word Break - Part 2 - GFG/word-break-part-2.cpp
+++ Word Break - Part 2 - GFG/word-break-part-2.cpp
@@ -9,30 +9,25 @@ using namespace std;
 
 class Solution{
 public:
-void solve(int ind, string s, vector<string> &ans, unordered_set<string> &st, string temp)
-{
-  if (ind == s.size())
-  {
-      temp.pop_back();
-    ans.push_back(temp);
-    return;
-  }
-
-  for (int i = ind; i < s.size(); i++)
-  {
-    string sub = s.substr(ind, i - ind + 1);
-    if (st.find(sub) != st.end())
-    {
-      solve(i + 1, s, ans, st, temp+sub+' ');
+void solve(int ind,string s,vector<string>&ans,unordered_set<string>&st,string temp){
+    if(ind==s.size()){
+        temp.pop_back();
+        ans.push_back(temp);
+        return;
     }
-  }
+    for(int i=ind;i<s.size();i++){
+        string sub=s.substr(ind,i-ind+1);
+        if(st.count(sub)){
+            solve(i+1,s,ans,st,temp+sub+" ");
+        }
+    }
 }
     vector<string> wordBreak(int n, vector<string>& dict, string s)
     {
         // code here
         vector<string>ans;
         unordered_set<string>st;
-        for(int i=0;i<dict.size();i++){
+        for(int i=0;i<n;i++){
             st.insert(dict[i]);
         }
         solve(0,s,ans,st,"");
