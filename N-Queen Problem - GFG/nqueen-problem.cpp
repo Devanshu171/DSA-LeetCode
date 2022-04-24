b8069b990c0980f6d4f4c30415971426bc42bb7b
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-24 17:40:57 +0530

    Added solution - LeetHub

--- N-Queen Problem - GFG/nqueen-problem.cpp
+++ N-Queen Problem - GFG/nqueen-problem.cpp
@@ -9,48 +9,48 @@ using namespace std;
 
 class Solution{
 public:
-bool isValid(int row,int col,int n,vector<vector<int>>&vis){
-    int i=row-1,j=col-1;
-    for(;i>=0 && j>=0;i--,j--){
-        if(vis[i][j]==1)
+bool isSafe(int row, int col,int n,vector<vector<int>>&board){
+    int i=row-1,j=col;
+    for(;i>=0;i--){
+        if(board[i][col]==1)
         return false;
     }
     
-    i=row-1;
-    for(;i>=0;i--){
-        if(vis[i][col]==1)
+    i=row-1,j=col-1;
+    for(;i>=0 && j>=0;i--,j--){
+        if(board[i][j]==1)
         return false;
     }
     i=row-1,j=col+1;
-    for(;i>=0 && j<n ;i--,j++){
-        if(vis[i][j]==1)
+    for(;i>=0 && j<n;i--,j++){
+        if(board[i][j]==1)
         return false;
     }
+    
     return true;
-}
-void solve(int row,int n,vector<vector<int>>&ans,vector<vector<int>>&vis,vector<int>&temp){
+} 
+void solve(int row,int n,vector<vector<int>>&board,vector<vector<int>>&ans,vector<int>temp){
     if(row==n){
         ans.push_back(temp);
         return;
     }
     for(int col=0;col<n;col++){
-        if(isValid(row,col,n,vis)){
-            vis[row][col]=1;
+        if(isSafe(row,col,n,board)){
+            board[row][col]=1;
             temp[col]=row+1;
-            solve(row+1,n,ans,vis,temp);
-            vis[row][col]=0;
-             temp[col]=-1;
+            solve(row+1,n,board,ans,temp);
+            temp[col]=-1;
+            board[row][col]=0;
         }
     }
 }
     vector<vector<int>> nQueen(int n) {
-        // code here
-        vector<vector<int>>ans,vis(n,vector<int>(n,0));
+        vector<vector<int>>board(n,vector<int>(n,0));
+        vector<vector<int>>ans;
         vector<int>temp(n,-1);
-        solve(0,n,ans,vis,temp);
+        solve(0,n,board,ans,temp);
         sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
 
