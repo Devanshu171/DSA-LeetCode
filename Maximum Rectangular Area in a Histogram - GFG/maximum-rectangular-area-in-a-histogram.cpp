f6635f93b14bb1eb9571cc26990046f03764a9a3
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-04-30 17:15:23 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -8,46 +8,42 @@ class Solution
 {
     public:
     //Function to find largest rectangular area possible in a given histogram.
-    void NSR(long long arr[],vector<long long >&sr,int n){
-        stack<long long>st;
-        int k=n-1;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            if(st.empty()) sr[k--]=n;
-            else sr[k--]=st.top();
-            st.push(i);
-        }
-        // for(auto it:sr) cout<<it<<" ";
-        // cout<<endl;
-        return;
+    #define ll long long
+    void nsl(ll arr[],int n,vector<ll>&left){
         
-    }
-    void NSL(long long arr[],vector<long long >&sl,int n){
-        stack<long long>st;
-        
+        stack<ll>st;
         for(int i=0;i<n;i++){
             while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            if(st.empty()) sl.push_back(-1);
-            else sl.push_back(st.top());
+            if(st.empty()) left.push_back(-1);
+            else left.push_back(st.top());
             st.push(i);
         }
-        // for(auto it:sl) cout<<it<<" ";
-        // cout<<endl;
-        return;
-        
+    }
+    void nsr(ll arr[],int n,vector<ll>&right){
+         stack<ll>st;
+         int m=n-1;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            if(st.empty()) right[m--]=n;
+            else right[m--]=st.top();
+            st.push(i);
+        }
     }
     long long getMaxArea(long long arr[], int n)
     {
         // Your code here
-        vector<long long>sr(n),sl;
-        NSR(arr,sr,n);
-        NSL(arr,sl,n);
-        long long maxArea=INT_MIN;
+        vector<ll>left,right(n);
+        nsl(arr,n,left);
+        nsr(arr,n,right);
+        
+        ll ans=0;
+        
         for(int i=0;i<n;i++){
-            long long area=(sr[i]-sl[i]-1)*arr[i];
-            maxArea=max(maxArea,area);
+            ll area=arr[i]*(right[i]-left[i]-1);
+            
+            ans=max(area,ans);
         }
-        return maxArea;
+        return ans;
     }
 };
 
