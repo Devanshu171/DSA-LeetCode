f395be734b9d1ea472d24991f2f7b7b3591a7f1d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-02 18:57:10 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -7,19 +7,18 @@ class Solution {
 	public:
 		string FirstNonRepeating(string s){
 		    // Code here
+		    queue<int>q;
 		    int count[26]={0};
-		    queue<char>q;
 		    for(int i=0;i<s.size();i++){
-		     count[s[i]-'a']++;
-		     if(count[s[i]-'a']==1){
-		         q.push(s[i]);
-		     }
-		     while(!q.empty() && count[q.front()-'a']!=1)
-		     q.pop();
-		     if(q.empty())
-		     s[i]='#';
-		    else
-		    s[i]=q.front();
+		        count[s[i]-'a']++;
+		        if(count[s[i]-'a']==1)
+		        q.push(s[i]);
+		        while(!q.empty() && count[q.front()-'a']!=1)
+		        q.pop();
+		        if(q.empty())
+		        s[i]='#';
+		        else
+		        s[i]=q.front();
 		    }
 		    return s;
 		}
