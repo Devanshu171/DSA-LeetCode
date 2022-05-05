baed8f37d0a3d82957d6d380f39c921b24eecca7
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-05 16:59:07 +0530

    Time: 186 ms (44.42%), Space: 21.3 MB (46.32%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -3,43 +3,47 @@ public:
     int minRemoval(string s){
         int op=0,cl=0;
         for(int i=0;i<s.size();i++){
-            // cout<<op<<" "<<cl<<endl;
             if(s[i]=='('){
                 op++;
             }else if(s[i]==')'){
-                if(op>0)
+                if(op>0){
                     op--;
-                else{
+                }else{
                     cl++;
                 }
-            }
+            }  
         }
-        // cout<<op<<" "<<cl<<endl;
-        return (op+cl);
+        return op+cl;
     }
-    void solve(string s,int count,vector<string>&ans,unordered_set<string>&st){
-      if(st.count(s))
-          return;
+    
+    void solve(int k,string s,vector<string>&ans,unordered_set<string>&st){
+        
+        if(st.count(s)){
+            return;
+        }
         st.insert(s);
-        if(count==0){
+        if(k==0){
             if(minRemoval(s)==0){
                 ans.push_back(s);
-               
             }
             return;
         }
-        for(int i=0;i<s.size();i++){
+     
+        
+        for(int i=0 ;i<s.size();i++){ 
             string left=s.substr(0,i);
             string right=s.substr(i+1);
-            solve(left+right,count-1,ans,st);
+            solve(k-1,left+right,ans,st);
         }
     }
     vector<string> removeInvalidParentheses(string s) {
-        
-        int count=minRemoval(s);
         vector<string>ans;
         unordered_set<string>st;
-        solve(s,count,ans,st);
+        
+        
+        int k=minRemoval(s);
+        solve(k,s,ans,st);
         return ans;
+        
     }
 };
