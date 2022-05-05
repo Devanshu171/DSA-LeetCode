012eb51ebea52cad41142d2c083a82a3ef21d23e
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-05 14:02:37 +0530

    Time: 3 ms (85.22%), Space: 10.9 MB (95.96%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,55 +1,30 @@
 class Solution {
 public:
-    int pivot(vector<int>&arr){
-        int s=0,e=arr.size()-1;
-        int n=arr.size();
-        // while(s<e){
-        //     int mid=s+(e-s)/2;
-        //     if(arr[mid]>=arr[0])
-        //         s=mid+1;
-        //     else
-        //         e=mid;
-        // }
-        // return s;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-           
-            int left=arr[(mid-1+n)%n];
-            int right=arr[(mid+1)%n];
-         cout<<mid<<endl;
-            if(arr[mid]>=right)
-                return (mid+1)%n;
-            else if(arr[mid]<=left)
-                return mid;
-            else if(arr[mid]>=arr[s]){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
-        }
-        return -1;
-    }
-    int bs(vector<int>&arr,int s,int e,int x){
-        
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(arr[mid]==x)
-                return mid;
-            else if(arr[mid]>x)
-                e=mid-1;
-            else
-                s=mid+1;
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
-        int pvt=pivot(nums);
-        cout<<pvt<<endl;
-        int ans1=bs(nums,0,pvt-1,target);
-        if(ans1!=-1)
-            return ans1;
-        else 
-            return bs(nums,pvt,nums.size()-1,target);
         
+        int start=0,end=nums.size()-1;
+        
+        while(start<=end){
+            
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+                return mid;
+            
+           else if(nums[start]<=nums[mid]){
+                if(target>=nums[start] && target<=nums[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(target>=nums[mid] && target<=nums[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+                
+        }
+        return -1;
     }
 };
