b002cddd9fbd8071e92038d8be39ae52d973d49b
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-07 18:02:42 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -5,20 +5,63 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 	public:
+	struct Node{
+	    char data;
+	    Node *next,*prev;
+	    Node(char x){
+	        data=x;
+	        next=NULL;
+	        prev=NULL;
+	    }
+	}*head=NULL,*tail=NULL;
+	void insert(Node *k){
+	    if(!head){
+	        head=k;
+	        tail=k;
+	    }else{
+	        k->prev=tail;
+	        tail->next=k;
+	        tail=k;
+	    }
+	}
+	void removeNode(Node *temp){
+	     if (head == NULL)
+        return;
+
+    if (head == temp)
+        head = head->next;
+    if (tail == temp)
+        tail = tail->prev;
+    if (temp->next != NULL)
+        temp->next->prev = temp->prev;
+    if (temp->prev != NULL)
+        temp->prev->next = temp->next;
+
+    delete (temp);
+	}
 		string FirstNonRepeating(string s){
 		    // Code here
-		    queue<int>q;
 		    int count[26]={0};
+		    unordered_map<char,Node*>mpp;
+		    
+		   
 		    for(int i=0;i<s.size();i++){
-		        count[s[i]-'a']++;
-		        if(count[s[i]-'a']==1)
-		        q.push(s[i]);
-		        while(!q.empty() && count[q.front()-'a']!=1)
-		        q.pop();
-		        if(q.empty())
+		        char x=s[i];
+		        count[x-'a']++;
+		        if(count[x-'a']==1){
+		            Node *temp=new Node(x);
+		            insert(temp);
+		            mpp[x]=temp;
+		        }
+		       else if(count[x-'a']>1 && mpp[x]!=NULL){
+		            removeNode(mpp[x]);
+		            mpp[x]=NULL;
+		        }
+		        
+		        if(!head)
 		        s[i]='#';
 		        else
-		        s[i]=q.front();
+		        s[i]=head->data;
 		    }
 		    return s;
 		}
