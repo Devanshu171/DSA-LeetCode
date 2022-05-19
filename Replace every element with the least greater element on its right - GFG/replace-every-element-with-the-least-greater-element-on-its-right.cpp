ea8d9122506d23c1668df6ecc0dc8641ce629188
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-19 22:31:18 +0530

    Added solution - LeetHub

---
+++ Replace every element with the least greater element on its right - GFG/replace-every-element-with-the-least-greater-element-on-its-right.cpp
@@ -0,0 +1,90 @@
+// { Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+    struct Node{
+        int data;
+        Node *left,*right;
+        Node(int temp){
+            data=temp;
+            left=right=NULL;
+        }
+    };
+    Node* InsertInBST(Node *&root,int x){
+        if(!root){
+            root=new Node(x);
+            return root;
+        }
+        
+        if(root->data>x){
+       root->left=InsertInBST(root->left,x);
+        }else{
+           root->right=InsertInBST(root->right,x);
+        }
+        
+        return root;
+          
+    }
+    int getUpperBound(Node *root,int x){
+        Node *cur=root;
+        int ans=-1;
+        while(cur){
+            if(cur->data>x){
+                ans=cur->data;
+                cur=cur->left;
+            }else{
+                cur=cur->right;
+            }
+        }
+        return ans;
+    }
+    void print(Node *root){
+        if(!root)
+        return;
+        print(root->left);
+        cout<<root->data<<" ";
+        print(root->right);
+    }
+    vector<int> findLeastGreater(vector<int>& arr, int n) {
+        
+        Node *root=NULL;
+        for(int i=n-1;i>=0;i--){
+           InsertInBST(root,arr[i]);
+            arr[i]=getUpperBound(root,arr[i]);
+        }
+        // print(root);
+        // cout<<endl;
+        return arr;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    vector<int>arr(n);
+	    for(int i = 0 ; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findLeastGreater(arr, n);
+	    for(auto it : ans){
+	        cout << it << " ";
+	    }
+	    cout <<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
