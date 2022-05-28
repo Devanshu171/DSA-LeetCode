25a4bd826ad255c08e3887ae244db93964ea0a73
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-28 15:12:23 +0530

    Time: 309 ms (46.11%), Space: 15.5 MB (46.18%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
-    int diff(string s2,string s1){
-        int cnt=0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i])
-                cnt++;
-        }
-        return cnt;
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-                queue<string>q;
-        q.push(beginWord);
+        
+        queue<string>q;
         unordered_set<string>st,words;
-        st.insert(beginWord);
         for(auto it:wordList){
             words.insert(it);
         }
+        q.push(beginWord);
+        st.insert(beginWord);
         int count=0;
         while(!q.empty()){
-             count++;
+            count++;
             int n=q.size();
             for(int i=0;i<n;i++){
-            
-            string s=q.front();
-            q.pop();
-                
-            if(s==endWord)
-                return count;
+                string s=q.front();
+                q.pop();
+                if(s==endWord)
+                    return count;
                 string t;
-            for(int i=0;i<s.size();i++){
-                t=s;
-                
-                for(char j='a';j<='z';j++){
-                    t[i]=j;
-                    if(words.count(t) && !st.count(t)){
-                        st.insert(t);
-                        q.push(t);
+                for(int i=0;i<s.size();i++){
+                    t=s;
+                    for(char j='a';j<='z';j++){
+                        t[i]=j;
+                        if(!st.count(t) && words.count(t)){
+                            st.insert(t);
+                            q.push(t);
+                        }
                     }
                 }
             }
-                
-           
-            }
-              
-         
         }
-           return 0;
+        
+        return 0;
     }
 };
