0cbb66bf22c52d5029e3afefb15f8b9807b82e30
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-05-29 22:21:24 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,79 +9,31 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	struct Edge{
-	    int u,v,w;
-	    Edge(int u,int v,int w){
-	        this->u=u;
-	        this->v=v;
-	        this->w=w;
-	    }
-	};
-	
-	class DSU{
-	    private:
-	    vector<int>parent,rank;
-	    public:
-	    DSU(int V){
-	        for(int i=0;i<=V;i++){
-	            parent.push_back(i);
-	            rank.push_back(0);
-	        }
-	    }
-	    
-	    int findPar(int u){
-	        if(parent[u]==u){
-	            return u;
-	        }
-	        
-	        return parent[u]=findPar(parent[u]);
-	    }
-	    
-	    void Union(int u,int v){
-	        u=findPar(u);
-	        v=findPar(v);
-	        if(u==v) return;
-	        
-	        if(rank[u]>rank[v]){
-	            parent[v]=u;
-	            rank[u]+=rank[v];
-            }else{
-                parent[u]=v;
-                rank[v]+=rank[u];
-            }
-	    }
-	};
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
+        vector<bool>vis(V,false);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,0});
         
-        vector<Edge>edges;
-        for(int i=0;i<V;i++){
-            for(auto it:adj[i]){
-                edges.push_back(Edge(i,it[0],it[1]));
+        int ans=0;
+        while(!pq.empty()){
+            int v=pq.top().second;
+            int w=pq.top().first;
+            pq.pop();
+            
+            if(vis[v]) continue;
+            
+            ans+=w;
+            vis[v]=true;
+            for(auto it:adj[v]){
+                if(!vis[it[0]]){
+                    pq.push({it[1],it[0]});
+                }
             }
         }
         
-        sort(edges.begin(),edges.end(),[&](Edge &a,Edge&b){
-            return a.w<b.w;
-        });
-        DSU dsu(V);
-        int ans=0;
-        // for(auto it:edges){
-        //     cout<<it.u<<"--"<<it.v<<endl;
-        // }
-        for(int i=0;i<edges.size();i++){
-           int u= edges[i].u;
-           int v=edges[i].v;
-           int w=edges[i].w;
-           if(dsu.findPar(u)!=dsu.findPar(v)){
-               ans+=w;
-               dsu.Union(u,v);
-           }
-        }
-        
         return ans;
-        
     }
 };
 
