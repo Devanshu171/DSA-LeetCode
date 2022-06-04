6f235343f8d3ce14b09b1fdba5ba8ce8b37c8109
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-04 18:03:58 +0530

    Added solution - LeetHub

---
+++ Water Connection Problem - GFG/water-connection-problem.cpp
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void dfs(int node,int start,int mini,vector<vector<int>>adj[],vector<vector<int>>&ans,vector<int>&vis,vector<int>&out){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it[0]] && out[it[0]]==1){
+                mini=min(mini,it[1]);
+                dfs(it[0],start,mini,adj,ans,vis,out);
+            }else if(out[it[0]]==0){
+                 mini=min(mini,it[1]);
+                ans.push_back({start,it[0],mini});
+                return;
+            }
+        }
+    }
+    vector<vector<int>> solve(int n,int p,vector<int> a,vector<int> b,vector<int> d)
+    {
+        // code here
+        vector<vector<int>>adj[n+1],ans;
+        vector<int>in(n+1,0),out(n+1,0),vis(n+1,0);
+        for(int i=0;i<p;i++){
+            in[b[i]]++;
+            out[a[i]]++;
+            adj[a[i]].push_back({b[i],d[i]});
+        }
+        int tap=0,tank=0;
+        for(int i=1;i<=n;i++){
+            if(in[i]==0 && out[i]!=0){
+                tank++;
+            }else if(in[i]!=0 && out[i]==0){
+                tap++;
+            }
+        }
+        vector<int>temp;
+        
+        for(int i=1;i<=n;i++){
+            if(in[i]==0 && out[i]!=0 && !vis[i]){
+                int mini=INT_MAX;
+                dfs(i,i,mini,adj,ans,vis,out);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t,n,p;
+	cin>>t;
+	while(t--)
+    {
+        cin>>n>>p;
+        vector<int> a(p),b(p),d(p);
+        for(int i=0;i<p;i++){
+            cin>>a[i]>>b[i]>>d[i];
+        }
+        Solution obj;
+        vector<vector<int>> answer = obj.solve(n,p,a,b,d);
+        cout<<answer.size()<<endl;
+        for(auto i:answer)
+        {
+            cout<<i[0]<<" "<<i[1]<<" "<<i[2]<<endl;
+        }
+        
+    }
+	return 0;
+}  // } Driver Code Ends
