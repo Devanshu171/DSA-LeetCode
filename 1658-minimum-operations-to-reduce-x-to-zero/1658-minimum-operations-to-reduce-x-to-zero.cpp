dbad6a532f1d5f9c199b4c4db3e9acd92816ec75
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-11 14:39:26 +0530

    Time: 216 ms (76.09%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int minOperations(vector<int>& n, int x) {
-         int size = n.size();
-          int total = 0;
-          int len = 0;
-          for(int i : n) total += i;
-          if(total == x) return size;
-          int need = total - x;
-          int i = 0 , j = 0;
-          int sum = 0;
-          while( j < size ) {
-                sum += n[j];  
-                while(i < j && sum > need){
-                        sum -= n[i++];
-                }
-                if(sum == need){
-                    
-                    len = max(len, j-i+1);
-                }
-                j++;
-          } 
-          if(len == 0) return -1;  
-          return size-len;
+    int minOperations(vector<int>& nums, int x) {
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int len=-1;
+        if(sum==x) return n;
+        int target=sum-x;
+        sum=0;
+        int i=0,j=0;
+        while(j<n){
+            sum+=nums[j];
+           while(i<j && sum>target){
+               sum-=nums[i++];
+           }
+            if(sum==target){
+                len=max(len,j-i+1);
+            }
+            j++;
+        }
+        return len==-1?-1:n-len;
     }
 };
