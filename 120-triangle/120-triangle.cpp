8225db633f59dead440647056220f961c182be35
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-13 18:53:56 +0530

    Time: 47 ms (5.45%), Space: 9.2 MB (9.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,20 +3,26 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         
-        vector<int>dp(triangle.back());
-        for(int i=n-2;i>=0;i--){
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--){
             for(int j=0;j<triangle[i].size();j++){
-              
-                dp[j]= triangle[i][j]+min( dp[j], dp[j+1]);
+                int left=0,right=0;
+                if(i+1<n){
+                   left= dp[i+1][j];                    
+                }
+                if(i+1<n && j+1<triangle[i+1].size()){
+                    right=dp[i+1][j+1];
+                }
+                dp[i][j]= triangle[i][j]+min(left,right);
             }
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<triangle[i].size();j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<triangle[i].size();j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
         
-        return dp[0];
+        return dp[0][0];
     }
 };
