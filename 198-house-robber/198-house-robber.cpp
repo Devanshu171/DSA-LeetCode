56ab2a40103fa9778d7493f18d7f24a71b729ee3
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-15 16:55:31 +0530

    Time: 5 ms (21.15%), Space: 7.7 MB (75.62%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int  solve(vector<int> &arr,int n,vector<int>&dp){
+//     int  solve(vector<int> &arr,int n,vector<int>&dp){
 
-            if(n==0) return arr[n];
-                if(dp[n]!=-1) return dp[n];
+//             if(n==0) return arr[n];
+//                 if(dp[n]!=-1) return dp[n];
             
-            int fs=arr[n];
-            if(n>1){
-                fs+=solve(arr,n-2 ,dp);
-            }
-            int ss=solve(arr,n-1,dp);
+//             int fs=arr[n];
+//             if(n>1){
+//                 fs+=solve(arr,n-2 ,dp);
+//             }
+//             int ss=solve(arr,n-1,dp);
             
-            return dp[n]=max(fs,ss);
+//             return dp[n]=max(fs,ss);
             
-        }
+//         }
     int rob(vector<int>& nums) {
             int n=nums.size();
     
-             vector<int>dp(n,-1);   
-        return solve(nums,n-1,dp);
+             // vector<int>dp(n,-1);   
+        // return solve(nums,n-1,dp);
+           vector<int>dp(n,0);   
+	   // return solve(arr,n-1,dp);
+	   dp[0]=nums[0];
+	   for(int i=1;i<n;i++){
+	       
+	       int withCur=nums[i];
+	       
+	       if(i>1) withCur+=dp[i-2];
+	       int withoutCur=dp[i-1];
+	       
+	       dp[i]=max(withoutCur,withCur);
+	   }
+	   
+	   return dp[n-1];
     }
 };
