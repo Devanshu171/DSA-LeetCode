c3c10a202cba38638d78cf471a95bd6024a4e429
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-15 17:58:14 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (97.24%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    	int findMaxSum(vector<int>&arr,int lo,int hi) {
+
+	   int prev1=0,prev2=0;
+	   for(int i=lo;i<=hi;i++){
+	       
+	       int withCur=arr[i];
+	       
+	       if(i>1) withCur+=prev2;
+	       int withoutCur=prev1;
+	       prev2=prev1;
+	       prev1=max(withoutCur,withCur);
+	   }
+	   
+	   return prev1;
+	}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[1],nums[0]);
+        return max(findMaxSum(nums,0,n-2),findMaxSum(nums,1,n-1));
+    }
+};
