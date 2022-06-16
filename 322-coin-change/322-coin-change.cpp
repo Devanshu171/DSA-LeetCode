864c3d04a20864c044f4b007e1328aa04be0826d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-17 01:43:05 +0530

    Time: 284 ms (9.42%), Space: 14.6 MB (37.17%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
-    	int solve(int x,vector<int>& coins,vector<int>&dp){
+int solveRec(int x,vector<int>& coins){
+    
+      if(x==0)  return 0;
+       if(x<0) return INT_MAX;
+
+    int mini=INT_MAX;
+    for(int i=0;i<coins.size();i++){
+        mini=min(mini,solveRec(x-coins[i],coins));
+    }
+    
+    return mini==INT_MAX?mini: 1+ mini;
+
+    }
+int solveMemo(int x,vector<int>& coins,vector<int>&dp){
     
       if(x==0)  return 0;
        if(x<0) return INT_MAX;
@@ -8,15 +21,35 @@ public:
     if(dp[x]!=-1) return dp[x];
     int mini=INT_MAX;
     for(int i=0;i<coins.size();i++){
-        mini=min(mini,solve(x-coins[i],coins,dp));
+        mini=min(mini,solveMemo(x-coins[i],coins,dp));
     }
     
     return dp[x]= mini==INT_MAX?mini: 1+ mini;
 
 }
+    int solveTabu(vector<int>& nums, int V){
+        vector<int>dp(V+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=V;i++){
+            
+            for(int j=0;j<nums.size();j++){
+                if(i-nums[j]>=0 && dp[i-nums[j]]!=INT_MAX){
+                    dp[i]=min(dp[i],dp[i-nums[j]]+1);
+                
+            }
+        }
+    }
+        
+        return dp[V];
+        
+    }
+    
     int coinChange(vector<int>& nums, int V) {
-        	    vector<int>dp(V+1,-1);
-    int ans=solve(V,nums,dp);
+        	    // vector<int>dp(V+1,-1);
+          // int ans=solveRec(V,nums);
+    // int ans=solveMemo(V,nums,dp);
+        int ans=solveTabu(nums,V);
+      
     return   ans==INT_MAX?-1:ans;
     }
 };
