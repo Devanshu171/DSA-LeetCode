f926fe780de75803679297a6a11bdde6e510842f
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-18 02:39:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (60.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,48 +1,69 @@
 class Solution {
 public:
 
-int helper(int i,int j,vector<vector<int>>&dp)
-{
-          if(i<0 || j<0)
-             return 0;
+// int helper(int i,int j,vector<vector<int>>&dp)
+// {
+//           if(i<0 || j<0)
+//              return 0;
     
-        if(i==0 && j==0)  
-            return 1;
-    if(dp[i][j]!=-1) return dp[i][j];
+//         if(i==0 && j==0)  
+//             return 1;
+//     if(dp[i][j]!=-1) return dp[i][j];
      
-   int l= helper(i-1,j,dp);
-   int r= helper(i,j-1,dp);
+//    int l= helper(i-1,j,dp);
+//    int r= helper(i,j-1,dp);
     
-    return dp[i][j]= l+r;
+//     return dp[i][j]= l+r;
     
-}
-    int solveTabu(int m,int n){
-                vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j]=1;
-                else{
-                    
+// }
+//     int solveTabu(int m,int n){
+//                 vector<vector<int>>dp(m,vector<int>(n,0));
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0) continue;
+
+//                 int down=0,right=0;
+//                 if(i>0)
+//                    down =dp[i-1][j];
+//                 if(j>0)
+//                     right=dp[i][j-1];
                 
+//                 dp[i][j]=right+down;
+                
+//             }
+//         }
+//         return dp[m-1][n-1];
+
+//     }
+    int solveTabuSo(int m,int n){
+                
+        vector<int>prev(n,0);
+      
+        for(int i=0;i<m;i++){
+            vector<int>temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) temp[j]=1;
+        else{
                 int down=0,right=0;
                 if(i>0)
-                   down =dp[i-1][j];
+                   down =prev[j];
                 if(j>0)
-                    right=dp[i][j-1];
+                    right=temp[j-1];
                 
-                // if(i!=0 && j!=0)
-                dp[i][j]=right+down;
+                temp[j]=right+down;
+                
                 }
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
 
     }
     int uniquePaths(int m, int n) {
         // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
             // return helper(m-1,n-1,dp);
-        return solveTabu(m,n);
+        return solveTabuSo(m,n);
     
     }
 };
