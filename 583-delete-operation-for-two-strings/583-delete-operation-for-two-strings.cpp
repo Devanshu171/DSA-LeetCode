58fdf77185a66834be90c036b11637856ab78d0b
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-17 18:40:32 +0530

    Time: 756 ms (9.64%), Space: 263.1 MB (9.21%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
-    // int solve(string w1,string w2,int i,int j){
-    //     while( j<w2.size() && i<w1.size() && w1[i]==w2[j]){
-    //         i++;j++;
+   
+    // int solveMemo(string w1,string w2,int i,int j,vector<vector<int>>&dp){
+    //     while( j>=0 && i>=0 && w1[i]==w2[j]){
+    //         i--;j--;
     //     }
-    //     if(i==w1.size() || j==w2.size()){
-    //         return max(w1.size()-i ,w2.size()-j);
+    //     if(i<0 || j<0){
+    //         return max(i+1 ,j+1);
     //     }
-    //     int ans=min(solve(w1,w2,i+1,j),solve(w1,w2,i,j+1));
-    //     return ans+1;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int ans=min(solveMemo(w1,w2,i-1,j,dp),solveMemo(w1,w2,i,j-1,dp));
+    //     return dp[i][j]=ans+1;
     // }
-    int solveMemo(string w1,string w2,int i,int j,vector<vector<int>>&dp){
-        while( j>=0 && i>=0 && w1[i]==w2[j]){
-            i--;j--;
+    // int minDistance(string w1, string w2) {
+    //     vector<vector<int>>dp(w1.size(),vector<int>(w2.size(),-1));
+    //     // return solve(w1,w2,0,0);
+    //     return solveMemo(w1,w2,w1.size()-1,w2.size()-1,dp);
+    // }
+    
+    int solve(string w1,string w2,int i,int j){
+        while( j<w2.size() && i<w1.size() && w1[i]==w2[j]){
+            i++;j++;
         }
-        if(i<0 || j<0){
-            return max(i+1 ,j+1);
+        if(i==w1.size() || j==w2.size()){
+            return max(w1.size()-i ,w2.size()-j);
+        }
+        int ans=min(solve(w1,w2,i+1,j),solve(w1,w2,i,j+1));
+        return ans+1;
+    }
+    int solveMemo(string w1,string w2,int i,int j,vector<vector<int>>&dp){
+        while( j<w2.size() && i<w1.size() && w1[i]==w2[j]){
+            i++;j++;
+        }
+        if(i==w1.size() || j==w2.size()){
+            return max(w1.size()-i ,w2.size()-j);
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        int ans=min(solveMemo(w1,w2,i-1,j,dp),solveMemo(w1,w2,i,j-1,dp));
+        int ans=min(solveMemo(w1,w2,i+1,j,dp),solveMemo(w1,w2,i,j+1,dp));
         return dp[i][j]=ans+1;
     }
     int minDistance(string w1, string w2) {
         vector<vector<int>>dp(w1.size(),vector<int>(w2.size(),-1));
-        // return solve(w1,w2,0,0);
-        return solveMemo(w1,w2,w1.size()-1,w2.size()-1,dp);
+        return solveMemo(w1,w2,0,0,dp);
     }
 };
