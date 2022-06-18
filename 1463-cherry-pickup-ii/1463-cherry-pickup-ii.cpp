398c38e1a0352d1223567d92989fa55534dbe363
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-19 03:03:05 +0530

    Time: 245 ms (12.20%), Space: 14.5 MB (20.88%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -41,9 +41,53 @@ public:
         }
         return dp[i][j1][j2]= maxi;
     }
+     int solveTabu(vector<vector<int>>& grid){
+         int n=grid.size();
+         int m=grid[0].size();
+         
+vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+         
+         for(int j1=0;j1<m;j1++){
+             for(int j2=0;j2<m;j2++){
+        if(j1==j2)    dp[n-1][j1][j2]=grid[n-1][j2];
+            else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];      
+             }
+         }
+         
+         for(int i=n-2;i>=0;i--){
+             for(int j1=0;j1<m;j1++){
+               for(int j2=0;j2<m;j2++){
+                   
+                        int maxi=0;
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                int cur=INT_MIN;
+                if(dj1+j1<m && dj1+j1>=0 && dj2+j2<m && dj2+j2>=0 )
+                 cur=dp[i+1][dj1+j1][dj2+j2];
+                
+                if(j1==j2) cur+=grid[i][j1];
+                else cur+=grid[i][j1]+grid[i][j2];
+                
+                
+                maxi=max(maxi,cur);
+            }
+        }
+         dp[i][j1][j2]= maxi;
+                   
+               }
+            }
+        }
+         int ans=INT_MIN;
+                    
+
+         return dp[0][0][m-1];
+     }
+         
+    
     int cherryPickup(vector<vector<int>>& grid) {
         // return solveRec(0,0,grid[0].size()-1,grid);
-        vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(grid[0].size(),-1)));
-        return solveMemo(0,0,grid[0].size()-1,grid,dp);
+        // vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(grid[0].size(),-1)));
+        // return solveMemo(0,0,grid[0].size()-1,grid,dp);
+        return solveTabu(grid);
     }
 };
