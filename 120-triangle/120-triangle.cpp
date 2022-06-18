32c1ccc76a2d52a46485ce4e4587f2a0cf262bfb
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-18 22:52:59 +0530

    Time: 13 ms (27.31%), Space: 9.2 MB (7.74%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,65 +1,50 @@
 class Solution {
 public:
-    int solveRec(int row,int indx,vector<vector<int>>& triangle){
-         if(indx<0 || indx==triangle[row].size()) return INT_MAX;
-        if(row==0){
-            return triangle[0][0];
+    int solveRec(int i,int j,vector<vector<int>>& triangle){
+         
+        if(i==triangle.size()-1){
+            return triangle[i][j];
         }
         
-        int fs=solveRec(row-1,indx,triangle);
-        int ss=solveRec(row-1,indx-1,triangle);
+        int fs=solveRec(i+1,j,triangle);
+        int ss=solveRec(i+1,j+1,triangle);
         
-        return min(fs,ss)+triangle[row][indx];
+        return min(fs,ss)+triangle[i][j];
         
     }
-     int solveMemo(int row,int indx,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-         if(indx<0 || indx==triangle[row].size()) return INT_MAX;
-        if(row==0){
-            return triangle[0][0];
-        }
-        if(dp[row][indx]!=INT_MAX) return dp[row][indx];
-        int fs=solveRec(row-1,indx,triangle);
-        int ss=solveRec(row-1,indx-1,triangle);
+     int solveMemo(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
         
-        return dp[row][indx]= min(fs,ss)+triangle[row][indx];
+        if(i==triangle.size()-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int fs=solveMemo(i+1,j,triangle,dp);
+        int ss=solveMemo(i+1,j+1,triangle,dp);
+        
+        return dp[i][j]= min(fs,ss)+triangle[i][j];
         
     }
-    int solveTabu(vector<vector<int>>& triangle){
-        int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
-        dp[0][0]=triangle[0][0];
+//     int solveTabu(vector<vector<int>>& triangle){
+//         int n=triangle.size();
+//                 vector<vector<int>>dp(n,vector<int>(n,0));
+//         for(int i=n-1,i>=0;i--){
+//             for(int j=0;j<i+1,j++){
+//                 if()
+//             }
+//         }
+
         
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i+1;j++){
-                int fs=INT_MAX;
-                int ss=INT_MAX;
-                 fs=dp[i-1][j];
-                if(j>0)   ss=dp[i-1][j-1];
-                
-                dp[i][j]=min(fs,ss)+triangle[i][j];
-            }
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            ans=min(ans,dp[n-1][i]);
-        }
-        return ans;
-    }
+        
+//     }
+   
     int minimumTotal(vector<vector<int>>& triangle) {
          
-        int ans=INT_MAX;
+   
         int n=triangle.size();
-        // for(int i=0;i<n;i++){
-        //     ans=min(ans,solveRec(n-1,i,triangle));
-        // }
-        // return ans;
-//         vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
-  
-//            for(int i=0;i<n;i++){
-//             ans=min(ans,solveMemo(n-1,i,triangle,dp));
-//         }
-//         return ans;
-        
-        return solveTabu(triangle);
+
+        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+
+      // return solveRec(0,0,triangle);
+        return solveMemo(0,0,triangle,dp);
     }
 };
