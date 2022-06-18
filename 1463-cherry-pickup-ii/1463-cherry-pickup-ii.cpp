4fcf0aacbd6d78447edd350463bd75c086d2a1dc
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-19 03:23:04 +0530

    Time: 132 ms (40.94%), Space: 8.8 MB (98.16%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -77,10 +77,52 @@ vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
                }
             }
         }
-         int ans=INT_MIN;
                     
 
          return dp[0][0][m-1];
+     }
+    int solveTabuSo(vector<vector<int>>& grid){
+         int n=grid.size();
+         int m=grid[0].size();
+        
+        // int front[m][m],curr[m][m];
+        vector<vector<int>>front(m,vector<int>(m,0));
+        vector<vector<int>>curr(m,vector<int>(m,0));
+          
+         for(int j1=0;j1<m;j1++){
+             for(int j2=0;j2<m;j2++){
+        if(j1==j2)    front[j1][j2]=grid[n-1][j2];
+            else front[j1][j2]=grid[n-1][j1]+grid[n-1][j2];      
+             }
+         }
+         
+         for(int i=n-2;i>=0;i--){
+             for(int j1=0;j1<m;j1++){
+               for(int j2=0;j2<m;j2++){
+                   
+                        int maxi=0;
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                int cur=INT_MIN;
+                if(dj1+j1<m && dj1+j1>=0 && dj2+j2<m && dj2+j2>=0 )
+                 cur=front[dj1+j1][dj2+j2];
+                
+                if(j1==j2) cur+=grid[i][j1];
+                else cur+=grid[i][j1]+grid[i][j2];
+                
+                
+                maxi=max(maxi,cur);
+            }
+        }
+         curr[j1][j2]= maxi;
+                   
+               }
+            }
+             front=curr;
+        }
+                    
+
+         return front[0][m-1];
      }
          
     
@@ -88,6 +130,7 @@ vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
         // return solveRec(0,0,grid[0].size()-1,grid);
         // vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(grid[0].size(),-1)));
         // return solveMemo(0,0,grid[0].size()-1,grid,dp);
-        return solveTabu(grid);
+        // return solveTabu(grid);
+        return solveTabuSo(grid);
     }
 };
