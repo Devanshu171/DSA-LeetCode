8d300304eb2df84978552d57b411b2f6f1744fc7
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-18 17:22:23 +0530

    Time: 17 ms (5.67%), Space: 7.9 MB (42.66%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -18,11 +18,30 @@ public:
         
         return dp[i][j]=up+left;
     }
+     int solveTabu(int m,int n,vector<vector<int>>&grid){
+                vector<vector<int>>dp(m,vector<int>(n,0));
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 if(grid[i][j]==1) continue;
+                 if(i==0 && j==0 && grid[i][j]!=1) dp[i][j]=1;
+                 else{
+                     int up=0,left=0;
+                     if(i>0) up=dp[i-1][j];
+                     if(j>0) left=dp[i][j-1];
+                     
+                     dp[i][j]=up+left;
+                 }
+             }
+         }
+         return dp[m-1][n-1];
+
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         // return solve(m-1,n-1,grid);
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solveMemo(m-1,n-1,grid,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return solveMemo(m-1,n-1,grid,dp);
+        return solveTabu(m,n,grid);
     }
 };
