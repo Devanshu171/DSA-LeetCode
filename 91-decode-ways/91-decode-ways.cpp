5a2a72ac82688020af67a5a0f063cad97dff438a
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-19 17:05:17 +0530

    Time: 6 ms (26.33%), Space: 6.1 MB (71.75%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -41,11 +41,34 @@ public:
         
       
     }
+    int solveTabuSo(string s){
+        
+        int n=s.size();
+        int prev1=1,prev2;
+     
+
+        
+        for(int i=n-1;i>=0;i--){
+            int cur=0;
+            if(s[i]=='0') cur=0;
+            else{
+             cur=prev1;
+        if(i<n-1 &&( s[i]=='1' || s[i]=='2' && s[i+1]<'7')) cur+=prev2;
+            }
+             prev2=prev1;
+                prev1=cur;
+        }
+        
+        return prev1;
+        
+      
+    }
     int numDecodings(string s) {
         
         // return solveRec(0,s);
         // vector<int>dp(s.size(),-1);
         // return solveMemo(0,s,dp);
-        return solveTabu(s);
+        // return solveTabu(s);
+        return solveTabuSo(s);
     }
 }; 
