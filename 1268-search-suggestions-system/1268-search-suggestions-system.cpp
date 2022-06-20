07af812833636c13857fc79835a773caec2bfbc3
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-20 15:17:02 +0530

    Time: 1964 ms (5.01%), Space: 659.9 MB (5.22%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,61 +1,80 @@
 class Solution {
 public:
-    pair<int,int> pvt(vector<string>& products, string searchWord,int i){
-           int s=0,e=products.size()-1;
-            int is=-1,ie=-1;
-              while(s<=e){
-            int mid= s+(e-s)/2;
-          string s1 = products[mid].substr(0,i+1);
-            string s2=searchWord.substr(0,i+1);
-            if(s1==s2){
-                is=mid;
-                e=mid-1;
-            }else if(s1<s2){
-                s=mid+1;
-            }else{
-                e=mid-1;
+    
+    struct Node{
+      Node *links[26];
+        bool flag=false;
+        
+        bool containsKey(char ch){
+            return (links[ch-'a']!=NULL);
+        }
+        
+        void put(char ch,Node *node){
+            links[ch-'a']=node;
+        }
+        
+        Node *get(char ch){
+            return links[ch-'a'];
+        }
+        void setEnd(){
+            flag=true;
+        }
+    };
+    
+    class Trie{
+      private: Node *root;
+      
+      public:
+        
+        Trie(){
+            root=new Node();
+        }
+        
+        void insert(string s){
+            Node* node=root;
+            for(int i=0;i<s.size();i++){
+            char ch=s[i];
+                if(!node->containsKey(ch)){
+                    node->put(ch,new Node());
+                }
+                node=node->get(ch);
+            }
+            node->setEnd();
+        }
+        void dfs(Node *node,string s,vector<string>&temp){
+            if(temp.size()==3) return;
+            if(!node) return;
+            
+            if(node->flag) temp.push_back(s);
+            for(char i='a';i<='z';i++){
+                if(node->containsKey(i)){
+                    dfs(node->get(i),s+i,temp);
+                }
             }
         }
-          s=0,e=products.size()-1;
-            while(s<=e){
-            int mid= s+(e-s)/2;
-          string s1 = products[mid].substr(0,i+1);
-            string s2=searchWord.substr(0,i+1);
-            if(s1==s2){
-                ie=mid;
-               s=mid+1;
-            }else if(s1<s2){
-                s=mid+1;
-            }else{
-                e=mid-1;
+        vector<string> getWords(string s){
+            Node *node=root;
+            for(int i=0;i<s.size();i++){
+                if(node->containsKey(s[i]))
+                node=node->get(s[i]);
+                else return {};
             }
+            vector<string>temp;
+            dfs(node,s,temp);
+            return temp;
         }
-        return {is,ie};
-    }
+    };
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(),products.end());
+        int n=products.size();
+                        Trie t;
+                for(int i=0;i<n;i++){
+                        t.insert(products[i]);
+                }
         vector<vector<string>>ans;
         for(int i=0;i<searchWord.size();i++){
-                            
-            pair<int,int> pts =pvt(products,searchWord,i);
-            int sindx=pts.first;
-            int eindx=pts.second;
-            
-  
-            vector<string>temp;
-            if(sindx!=-1){
-                int cnt=1;
-           
-            while(sindx<=eindx&& cnt<=3){
-                temp.push_back(products[sindx]);
-                sindx++;
-                cnt++;
-            }
-            }
-                ans.push_back(temp);
-        
+            ans.push_back(t.getWords(searchWord.substr(0,i+1)));
         }
-        
         return ans;
+        
     }
 };
