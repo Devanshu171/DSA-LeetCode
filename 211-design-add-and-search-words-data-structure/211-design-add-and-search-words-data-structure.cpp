91de7a3cf4b48ebe48adb663d747e83c4141a18a
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-20 14:10:09 +0530

    Time: 1590 ms (47.98%), Space: 558.5 MB (50.86%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,98 +1,53 @@
+class TrieNode {
+public:
+    bool word;
+    TrieNode* children[26];
+    TrieNode() {
+        word = false;
+        memset(children, NULL, sizeof(children));
+    }
+};
+
 class WordDictionary {
 public:
-    struct Node{
-     Node *links[26];
-        bool flag=false;
-        
-        bool containsKey(char ch){
-            return (links[ch-'a']!=NULL);
-        }
-        Node *get(char ch){
-            return links[ch-'a'];
-        }
-        void put(char ch,Node *node){
-            links[ch-'a']=node;
-        }
-        void setEnd(){
-            flag=true;
-        }
-    };
-    
-    class Trie{
-      
-        private:  Node *root;
-        
-        public:
-      
-        Trie(){
-            root=new Node();
-        }
-
-        void insert(string s){
-            Node *node=root;
-            
-            for(int i=0;i<s.size();i++){
-                char ch=s[i];
-                if(!node->containsKey(ch)){
-                    node->put(ch,new Node());
-                }
-                node=node->get(ch);
-            }
-            node->setEnd();
-        }
-     
-     
-        bool searchAll(string &s,Node *node,int i){
-
-           for(;i<s.size();i++){
-               char ch=s[i];
-              if(ch=='.'){
-                  for(int j='a';j<='z';j++){
-                      if(node->containsKey(j)){
-                          if(searchAll(s,node->get(j),i+1))
-                              return true;
-                      }
-                  }
-                  return false;
-                  
-              }else{
-                  
-               if(node->containsKey(ch)){
-                   node=node->get(ch);
-               }else{
-                   return false;
-               }
-                  
-             }
-          }
-            
-                       if(i==s.size()) return node->flag;
-                     else    return false;
-            
-    }
-        bool search(string &word){
-            Node *node=root;
-            return searchAll(word,root,0);
-        }
-    };
-    Trie t;
+    /** Initialize your data structure here. */
     WordDictionary() {
         
     }
     
+    /** Adds a word into the data structure. */
     void addWord(string word) {
-        t.insert(word);
+        TrieNode* node = root;
+        for (char c : word) {
+            if (!node -> children[c - 'a']) {
+                node -> children[c - 'a'] = new TrieNode();
+            }
+            node = node -> children[c - 'a'];
+        }
+        node -> word = true;
     }
     
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     bool search(string word) {
-        // Node *node=getRoot();
-        return t.search(word);
+        return search(word.c_str(), root);
+    }
+private:
+    TrieNode* root = new TrieNode();
+    
+    bool search(const char* word, TrieNode* node) {
+        for (int i = 0; word[i] && node; i++) {
+            if (word[i] != '.') {
+                node = node -> children[word[i] - 'a'];
+            } else {
+                TrieNode* tmp = node;
+                for (int j = 0; j < 26; j++) {
+                    node = tmp -> children[j];
+                    if (search(word + i + 1, node)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return node && node -> word;
     }
 };
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary* obj = new WordDictionary();
- * obj->addWord(word);
- * bool param_2 = obj->search(word);
- */
