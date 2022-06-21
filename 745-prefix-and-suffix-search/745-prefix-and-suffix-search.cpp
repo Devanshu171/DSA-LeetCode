7f4d304d6ac901cebd2ab97f4f852d1a9958022f
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-21 17:16:54 +0530

    Time: 879 ms (56.32%), Space: 404.3 MB (5.00%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,25 +1,79 @@
 class WordFilter {
 public:
-    
-unordered_map<string,int>mpp;
-  WordFilter(vector<string> &words)
-{
-    for(int i=0;i<words.size();i++){
-        string p="";
-        for(int j=0;j<words[i].size();j++){
-                 p+=words[i][j];
-                    
-                string s="";
-             for(int k=words[i].size()-1;k>=0;k--){
-                 s=words[i][k]+s;
-                 mpp[p+'.'+s]=i;
-             }         
+    struct Node{
+      Node *links[26];
+        vector<int>indices;
+        
+        bool containsKey(char ch){
+            return (links[ch-'a']!=NULL);
         }
+        
+        void put(char ch,Node *node){
+            links[ch-'a']=node;
+        }
+        
+        Node *get(char ch){
+            return links[ch-'a'];
+        }
+        void setEnd(){
+            // flag=true;
+        }
+    };
+    
+    class Trie{
+      private: Node *root;
+      
+      public:
+        
+        Trie(){
+            root=new Node();
+        }
+        
+        void insert(string s,int ind){
+            Node* node=root;
+            for(int i=0;i<s.size();i++){
+            char ch=s[i];
+                if(!node->containsKey(ch)){
+                    node->put(ch,new Node());
+                }
+                node->indices.push_back(ind);
+                node=node->get(ch);
+            }
+             node->indices.push_back(ind);
+           
+        }
+        vector<int> search(string prefix, string suffix){
+            Node *node=root;
+            for(int i=0;i<prefix.size();i++){
+                if(!node->containsKey(prefix[i])) return {};
+                node=node->get(prefix[i]);
+            }
+          return node->indices; 
+        }
+        
+    };
+    Trie t;
+    vector<string>ws;
+    WordFilter(vector<string>& words) {
+     for(int i=0;i<words.size();i++){
+         t.insert(words[i],i);
+     }   
+        ws=words;
     }
-}
+    
     int f(string prefix, string suffix) {
-        if(mpp.find(prefix+'.'+suffix)==mpp.end()) return -1;
-    return mpp[prefix+'.'+suffix];
+        
+        vector<int>indi=t.search(prefix,suffix);
+        
+        for(int i=indi.size()-1;i>=0;i--){
+            int j=suffix.size()-1,k=ws[indi[i]].size()-1;
+            while(j>=0 && k>=0 && suffix[j]==ws[indi[i]][k]){
+                k--;j--;
+            }
+            if(j==-1) return indi[i];
+            
+        }
+        return -1;
     }
 };
 
