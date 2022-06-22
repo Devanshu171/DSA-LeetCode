56e98a1bdccd785fa540588043ec05daa6b6c28f
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-22 13:05:37 +0530

    Time: 432 ms (25.73%), Space: 11.8 MB (62.10%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -20,6 +20,25 @@ bool solveMemo(int ind,int target,vector<int>&arr,vector<vector<int>>&dp){
     
     return dp[ind][target]= (take||notTake);
 }
+    bool solveTabu(int sum,vector<int>&arr){
+        int n=arr.size();
+    vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(arr[0]<=sum) dp[0][arr[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                 bool notTake=dp[i-1][target];
+                    bool take=false;
+                    if(arr[i]<=target) take=dp[i-1][target-arr[i]];
+
+                     dp[i][target]= (take||notTake);
+            }
+        }
+                                               
+        return dp[n-1][sum];
+        
+}
 
     bool canPartition(vector<int>& arr) {
             int sum=0,n=arr.size();
@@ -27,7 +46,8 @@ bool solveMemo(int ind,int target,vector<int>&arr,vector<vector<int>>&dp){
     if(sum%2!=0) return false;
         
 //     return solveRec(n-1,sum/2,arr);
-    vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
-    return solveMemo(n-1,sum/2,arr,dp);
+    // vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
+    // return solveMemo(n-1,sum/2,arr,dp);
+        return solveTabu(sum/2,arr);
     }
 };
