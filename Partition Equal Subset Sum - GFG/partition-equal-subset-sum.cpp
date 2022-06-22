1d73b0b7e0b6d4989dafb6e0d231b48e5aa7ee9a
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-22 13:26:31 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+ bool solveRec(int ind,int target,vector<int>&arr){
+    if(target==0) return true;
+    if(ind==0) return arr[ind]==target;
+    
+    bool notTake=solveRec(ind-1,target,arr);
+    bool take=false;
+    if(arr[ind]<=target) take=solveRec(ind-1,target-arr[ind],arr);
+    
+    return (take||notTake);
+}
+
+bool solveMemo(int ind,int target,vector<int>&arr,vector<vector<int>>&dp){
+    if(target==0) return true;
+    if(ind==0) return arr[ind]==target;
+    if(dp[ind][target]!=-1) return dp[ind][target];
+    bool notTake=solveMemo(ind-1,target,arr,dp);
+    bool take=false;
+    if(arr[ind]<=target) take=solveMemo(ind-1,target-arr[ind],arr,dp);
+    
+    return dp[ind][target]= (take||notTake);
+}
+
+    bool solveTabu(int sum,vector<int>&arr){
+        int n=arr.size();
+    vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(arr[0]<=sum) dp[0][arr[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                 bool notTake=dp[i-1][target];
+                    bool take=false;
+                    if(arr[i]<=target) take=dp[i-1][target-arr[i]];
+
+                     dp[i][target]= (take||notTake);
+            }
+        }
+                                               
+        return dp[n-1][sum];
+        
+}
+
+    bool solveTabuSo(int sum,vector<int>&arr){
+        int n=arr.size();
+    vector<bool>prev(sum+1,false),cur(sum+1,false);
+        prev[0]=cur[0]=true;
+        if(arr[0]<=sum) prev[arr[0]]=cur[arr[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                 bool notTake=prev[target];
+                    bool take=false;
+                    if(arr[i]<=target) take=prev[target-arr[i]];
+
+                    cur[target]= (take||notTake);
+            }
+            prev=cur;
+        }
+                                               
+        return prev[sum];
+        
+}
+
+    int equalPartition(int n, int nums[])
+    {
+        vector<int>arr;
+        for(int i=0;i<n;i++) arr.push_back(nums[i]);
+        int sum=0;
+    for(auto it:arr) sum+=it;
+    if(sum%2!=0) return false;
+        
+    //     return solveRec(n-1,sum/2,arr);
+    // vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
+    //     return solveMemo(n-1,sum/2,arr,dp);
+    //     return solveTabu(sum/2,arr);
+        return solveTabuSo(sum/2,arr);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}  // } Driver Code Ends
