d78f570e02a4e9454dbd95072e52a629d0f9de07
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-24 23:34:16 +0530

    Time: 3 ms (87.41%), Space: 9.4 MB (94.96%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -17,7 +17,8 @@ public:
         
     }
      int solveMemo(int ind,int pass,vector<int>&days,vector<int>&costs,vector<int>&dp){
-      
+      // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Minimum Cost For Tickets.
+       // Memory Usage: 9.6 MB, less than 70.83% of C++ online submissions for Minimum Cost For Tickets.
        if(ind>=days.size()) return 0;
         if(dp[pass]!=-1) return dp[pass];
         if(pass<days[ind]){
@@ -32,31 +33,31 @@ public:
   
         
     }
-    int solveTabu(vector<int>& days, vector<int>& costs){
-        int n=days.size();
-        vector<vector<int>>dp(n+1,vector<int>(365+31,1e8));
-        for(int i=0;i<=365+30;i++) dp[n+1][i]=0;
+  int solveTabu(vector<int>& DAYS, vector<int>& cost) {
+        int k = 0;
+        vector<int> dp(366);
         
-        for(int ind=n-1;ind>=0;ind--){
-            for(int pass=365;pass>=0;pass--){
-                 if(pass<days[ind]){
-                
-                     int pass1=dp[ind+1][days[ind]]+costs[0];
-                     int pass2=dp[ind+1][days[ind]+6]+costs[1];
-                     int pass3=dp[ind+1][days[ind]+29]+costs[2];
-                
-              dp[ind][pass]=min(pass1,min(pass2,pass3));
-            
-        }
-         // dp[ind][pass]=solveMemo(ind+1,pass,days,costs,dp);
+        for(int i=1 ; i<366 ; i++)
+        {
+            dp[i] = dp[i-1];
+            if(i == DAYS[k])
+            {
+			// cost of ith day , decision of ith day will be
+                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
+                             dp[max(0, i - 7)] + cost[1],  // 2nd case
+                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
+                if(k<DAYS.size()-1)
+                    k++;
             }
         }
-        return dp[0][0];
+        return dp[365];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // return solveRec(0,0,days,costs);
-        vector<int>dp(365+31,-1);
-        return solveMemo(0,0,days,costs,dp);
-        // return solveTabu(days,costs);
+        // vector<int>dp(365+31,-1);
+        // return solveMemo(0,0,days,costs,dp);
+        return solveTabu(days,costs);
+        
     }
+   
 };
