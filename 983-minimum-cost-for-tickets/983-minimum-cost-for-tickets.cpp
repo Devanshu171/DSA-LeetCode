b5234d3fda2dbb940b43f558c1f1f0209ffc79dc
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-24 23:35:58 +0530

    Time: 11 ms (25.58%), Space: 9.5 MB (70.83%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -33,31 +33,27 @@ public:
   
         
     }
-  int solveTabu(vector<int>& DAYS, vector<int>& cost) {
-        int k = 0;
-        vector<int> dp(366);
-        
-        for(int i=1 ; i<366 ; i++)
-        {
+    int solveTabu(vector<int>& days, vector<int>& costs){
+        int n=days.size();
+        vector<int>dp(366);
+   int k=0;
+        for(int i=1;i<=365;i++){
+            
             dp[i] = dp[i-1];
-            if(i == DAYS[k])
-            {
-			// cost of ith day , decision of ith day will be
-                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
-                             dp[max(0, i - 7)] + cost[1],  // 2nd case
-                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
-                if(k<DAYS.size()-1)
-                    k++;
+            if(k<n && i==days[k] ){
+                dp[i]=min({dp[i-1]+costs[0],
+                          dp[max(0,i-7)]+costs[1],
+                          dp[max(0,i-30)]+costs[2]});
+                          k++;
             }
+            
         }
         return dp[365];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // return solveRec(0,0,days,costs);
-        vector<int>dp(365+31,-1);
-        return solveMemo(0,0,days,costs,dp);
+        // vector<int>dp(365+31,-1);
+        // return solveMemo(0,0,days,costs,dp);
         return solveTabu(days,costs);
-        
     }
-   
 };
