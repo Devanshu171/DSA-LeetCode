0765ac0b66e57b2baffd9aabf837211a83ec45a4
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-24 15:40:15 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -25,19 +25,41 @@ int solveMemo(int ind,int tar,int nums[],vector<vector<int>>&dp){
         if(nums[0]<=tar && tar%nums[0]==0) return tar/nums[0];
         else return 1e8;
     }
+    
     if(dp[ind][tar]!=-1) return dp[ind][tar];
+    
     int notPick=solveMemo(ind-1,tar,nums,dp);
     int pick=1e8;
     if(tar>=nums[ind]) pick=1+solveMemo(ind,tar-nums[ind],nums,dp);
     
     return dp[ind][tar]= min(notPick,pick);
 }
+int solveTabu(int nums[], int M, int V){
+    vector<vector<int>>dp(M,vector<int>(V+1,1e8));
+    for(int i=0;i<M;i++) dp[i][0]=0;
+    for(int i=nums[0];i<=V;i++){
+        if(i%nums[0]==0) dp[0][i]=i/nums[0];
+    }
+    
+    for(int ind=1;ind<M;ind++){
+        for(int tar=1;tar<=V;tar++){
+             int notPick=dp[ind-1][tar];
+                 int pick=1e8;
+                if(tar>=nums[ind]) pick=1+dp[ind][tar-nums[ind]];
+    
+             dp[ind][tar]=min(notPick,pick);
+        }
+    }
+    
+    return dp[M-1][V];
+}
 	int minCoins(int nums[], int M, int V) 
 	{ 
 	    // Your code goes here4
 	   // int ans= solveRec(M-1,V,nums);
 	    vector<vector<int>>dp(M,vector<int>(V+1,-1));
-	    int ans=solveMemo(M-1,V,nums,dp);
+	   // int ans=solveMemo(M-1,V,nums,dp);
+	   int ans=solveTabu(nums,M,V);
 	    return ans==1e8?-1:ans;
   
 	} 
