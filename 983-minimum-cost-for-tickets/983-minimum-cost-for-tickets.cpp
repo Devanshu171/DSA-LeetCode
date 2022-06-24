1a323ecf55b6e97ca5b7f0bcb5d5fc3a8b2bb9d8
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-24 23:16:01 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (70.83%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -16,25 +16,47 @@ public:
   
         
     }
-     int solveMemo(int ind,int pass,vector<int>&days,vector<int>&costs,vector<vector<int>>&dp){
+     int solveMemo(int ind,int pass,vector<int>&days,vector<int>&costs,vector<int>&dp){
       
        if(ind>=days.size()) return 0;
-        if(dp[ind][pass]!=-1) return dp[ind][pass];
+        if(dp[pass]!=-1) return dp[pass];
         if(pass<days[ind]){
-
+ 
      int pass1=solveMemo(ind+1,days[ind],days,costs,dp)+costs[0];
      int pass2=solveMemo(ind+1,days[ind]+6,days,costs,dp)+costs[1];
      int pass3=solveMemo(ind+1,days[ind]+29,days,costs,dp)+costs[2];
-            return  dp[ind][pass]=min(pass1,min(pass2,pass3));
+            return  dp[pass]=min(pass1,min(pass2,pass3));
             
         }
-        return dp[ind][pass]=solveMemo(ind+1,pass,days,costs,dp);
+        return dp[pass]=solveMemo(ind+1,pass,days,costs,dp);
   
         
     }
+    int solveTabu(vector<int>& days, vector<int>& costs){
+        int n=days.size();
+        vector<vector<int>>dp(n+1,vector<int>(365+31,1e8));
+        for(int i=0;i<=365+30;i++) dp[n+1][i]=0;
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int pass=365;pass>=0;pass--){
+                 if(pass<days[ind]){
+                
+                     int pass1=dp[ind+1][days[ind]]+costs[0];
+                     int pass2=dp[ind+1][days[ind]+6]+costs[1];
+                     int pass3=dp[ind+1][days[ind]+29]+costs[2];
+                
+              dp[ind][pass]=min(pass1,min(pass2,pass3));
+            
+        }
+         // dp[ind][pass]=solveMemo(ind+1,pass,days,costs,dp);
+            }
+        }
+        return dp[0][0];
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // return solveRec(0,0,days,costs);
-        vector<vector<int>>dp(days.size(),vector<int>(365+31,-1));
+        vector<int>dp(365+31,-1);
         return solveMemo(0,0,days,costs,dp);
+        // return solveTabu(days,costs);
     }
 };
