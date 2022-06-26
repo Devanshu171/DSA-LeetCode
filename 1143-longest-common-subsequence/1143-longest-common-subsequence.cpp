1c57ba4123191d285837196d1ea9cb80b23a3be0
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-06-26 14:42:52 +0530

    Time: 32 ms (54.73%), Space: 6.5 MB (93.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -39,11 +39,30 @@ public:
         }
         return dp[n][m];
     }
+      int solveTabuSo(string s1,string s2){
+        int n=s1.size(),m=s2.size();
+        vector<int>prev(m+1,0),cur(m+1,0);
+  
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                
+                   if(s1[ind1-1]==s2[ind2-1]){
+              cur[ind2]=1+prev[ind2-1];
+               }else{
+                      cur[ind2]=max(prev[ind2],cur[ind2-1]);
+                     }
+              }
+            prev=cur;
+        }
+        return prev[m];
+    }
     int longestCommonSubsequence(string s1, string s2) {
             int n=s1.size(),m=s2.size();
         // return solveRec(n-1,m-1,s1,s2);
         // vector<vector<int>>dp(n,vector<int>(m,-1));
         // return solveMemo(n-1,m-1,s1,s2,dp);
-        return solveTabu(s1,s2);
+        // return solveTabu(s1,s2);
+        return solveTabuSo(s1,s2);
     }
 };
