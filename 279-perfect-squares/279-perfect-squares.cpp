c195b932d8ce9736401e5257270e7d6a3ef288e0
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-01 16:03:22 +0530

    Time: 260 ms (52.74%), Space: 11.5 MB (21.76%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -5,7 +5,7 @@ public:
         if(n<0) return 1e8;
         
         int ans=INT_MAX;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i*i<=n;i++){
             ans=min(ans,solveRec(n-i*i)+1);
         }
         
@@ -27,9 +27,9 @@ public:
         
         dp[0]=0;
         for(int i=1;i<=n;i++){
-     
-             for(int j=1;j<=n;j++){
-          if(j*j<=i) dp[i]=min(dp[i],dp[n-j*j]+1);
+         
+             for(int j=1;j*j<=n;j++){
+          if(i>=j*j) dp[i]=min(dp[i],dp[i-j*j]+1);
             }
         
         }
@@ -38,7 +38,7 @@ public:
     int numSquares(int n) {
         vector<int>dp(n+1,-1);
         // return solveRec(n);
-        return solveMemo(n,dp);
+        // return solveMemo(n,dp);
         return solveTabu(n);
 
     }
