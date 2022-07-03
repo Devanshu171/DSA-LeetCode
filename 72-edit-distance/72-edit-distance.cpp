5fbe571a2dc1cde4fbeeddbfc4896b647d53e3b0
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-03 13:16:00 +0530

    Time: 17 ms (63.96%), Space: 9.1 MB (37.94%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -28,10 +28,42 @@ public:
             return dp[ind1][ind2]= min(remove,min(replace,insert));
         }
     }
+    int solveTabu(string s1, string s2){
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=0;i<=m;i++)
+            dp[0][i]=i;
+           
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1[ind1-1]==s2[ind2-1]){
+             dp[ind1][ind2]= dp[ind1-1][ind2-1];
+        }else{
+            int remove=dp[ind1-1][ind2]+1;
+              int insert=dp[ind1][ind2-1]+1;
+            int replace=dp[ind1-1][ind2-1]+1;
+            
+             dp[ind1][ind2]= min(remove,min(replace,insert));
+        }
+            }
+        }
+        
+        return dp[n][m];
+           
+        
+        
+
+    }
     int minDistance(string s1, string s2) {
         int n=s1.size(),m=s2.size();
         // return solveRec(s1.size()-1,s2.size()-1,s1,s2);
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solveMemo(n-1,m-1,s1,s2,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return solveMemo(n-1,m-1,s1,s2,dp);
+        return solveTabu(s1,s2);
     }
 };
