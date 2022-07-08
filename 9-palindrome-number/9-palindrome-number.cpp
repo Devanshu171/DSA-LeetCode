9a66ff4ce0aa4b36a84045ea9a8dea129de2cb79
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-08 22:30:00 +0530

    Time: 22 ms (51.87%), Space: 5.9 MB (31.70%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
-        vector<int>vec;
-        while(x){
-            vec.push_back(x%10);
-            x/=10;
+        int y=x;
+        long long r=0;
+        while(y){
+            r=r*10+y%10;
+            y/=10;
+            
         }
-        for(int i=0;i<vec.size();i++){
-            if(vec[i]!=vec[vec.size()-i-1]) return false;
-        }
-        return true;
+        return r==x;
     }
 };
