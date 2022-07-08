3888fddf2ac852194dacb56325343b3829cfe7c9
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-09 00:08:42 +0530

    Added solution - LeetHub

--- Convert to Roman No - GFG/convert-to-roman-no.cpp
+++ Convert to Roman No - GFG/convert-to-roman-no.cpp
@@ -15,29 +15,8 @@ class Solution{
         string hund[]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         string thus[]={"","M","MM","MMM"};
        
-        string ans="";
-        int place=0;
-        while(num){
-            if(num%10==0){
-                while(num%10==0) {
-                    place++;
-                    num/=10;
-            }   
-         }else{
-                 if(place==0){
-                ans=ones[num%10]+ans;
-            }else if(place==1){
-                ans=tens[num%10]+ans;
-            }else if(place==2){
-                ans=hund[num%10]+ans;
-            }else{
-                ans=thus[num%10]+ans;
-            }
-                  num/=10;
-                place++;
-            }
-          
-        }
+        string ans=thus[num/1000]+hund[num%1000/100]+tens[num%100/10]+ones[num%10];
+       
         return ans;
     }
 };
