94cbff2f42d11d8f8038b1bd06bb78c242a7aac8
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-15 10:07:31 +0530

    Time: 36 ms (35.70%), Space: 24.1 MB (25.82%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    bool isvalid(int i,int j,int n,int m,vector<vector<int>>&grid){
-        if(i>=0 && i<n && j<m && j>=0 && grid[i][j]==1) return true;
-        return false;
+    bool isValid(int x, int y, int n, int m, vector<vector<int>> &grid, vector<vector<int>> &vis){
+    return (x>=0 && y>=0 && x<n && y<m && grid[x][y]==1 && vis[x][y]==0);
+} 
+
+int solve(int x, int y, int n, int m, vector<vector<int>> &grid, vector<vector<int>> &vis)
+{
+  int ans=0;
+  
+  int di[]={1,-1,0,0};
+  int dj[]={0,0,1,-1};
+  vis[x][y]=1;
+
+  for(int i=0;i<4;i++){
+    int newX=x+di[i];
+    int newY=y+dj[i];
+    if (isValid(newX, newY, n, m, grid, vis))
+    {
+      ans+=solve(newX,newY,n,m,grid,vis);
     }
-    void dfs(int i,int j,int n,int m,int &area,vector<vector<int>>&grid){
-        area++;
-        grid[i][j]=2;
-        int di[]={1,-1,0,0};
-        int dj[]={0,0,-1,1};
-        
-        for(int k=0;k<4;k++){
-            int col=i+di[k];
-            int row=j+dj[k];
-            if(isvalid(col,row,n,m,grid)){
-                dfs(col,row,n,m,area,grid);
-            }
-        }
-        
-    }
+  }
+
+  return ans+1;
+}
+
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int maxArea=0;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int area=0;
-                if(grid[i][j]==1){
-                    dfs(i,j,n,m,area,grid);
-                    maxArea=max(area,maxArea);
-                }
-            }
-        }
-        return maxArea;
+  int n=grid.size();
+  int m=grid[0].size();
+  vector<vector<int>>vis(n,vector<int>(m,0));
+  int maxArea=0;
+
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(grid[i][j]==1 && vis[i][j]!=1){
+        int area=solve(i,j,n,m,grid,vis);
+        maxArea=max(area,maxArea);
+      }
+    }
+  }
+
+  return maxArea;
     }
 };
