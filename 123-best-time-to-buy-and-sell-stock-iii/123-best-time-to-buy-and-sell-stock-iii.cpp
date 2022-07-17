33139af5cdf5d74c0316a26889a2f4ab5809e061
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 19:27:58 +0530

    Time: 1324 ms (10.43%), Space: 218 MB (12.83%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
+    int solveRec(int ind,int buy,int trans,vector<int>&prices){
+        if(ind==prices.size()) return 0;
+        if(trans==0) return 0;
+        
+        int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+solveRec(ind+1,0,trans,prices),solveRec(ind+1,1,trans,prices));
+        }else{
+             profit=max(prices[ind]+solveRec(ind+1,1,trans-1,prices),solveRec(ind+1,0,trans,prices));
+        }
+        return profit;
+    }
+     int solveMemo(int ind,int buy,int trans,vector<int>&prices,vector<vector<vector<int>>>&dp){
+        if(ind==prices.size()) return 0;
+        if(trans==0) return 0;
+        if(dp[ind][buy][trans]!=-1) return dp[ind][buy][trans];
+        int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+solveMemo(ind+1,0,trans,prices,dp),solveMemo(ind+1,1,trans,prices,dp));
+        }else{
+             profit=max(prices[ind]+solveMemo(ind+1,1,trans-1,prices,dp),solveMemo(ind+1,0,trans,prices,dp));
+        }
+        return dp[ind][buy][trans]= profit;
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        int profit[n];
-        int buy=INT_MAX;
-      int  cp=INT_MIN;
-        int mxi=cp;
-        
-        for(int i=0;i<n;i++){
-            buy=min(buy,prices[i]);
-            cp=max(cp,prices[i]-buy);
-            mxi=max(mxi,cp);
-            profit[i]=mxi;
-        }
-        int sell=INT_MIN;
-        cp=INT_MIN;
-        mxi=cp;
-        int ans=-1;
-        for(int i=n-1;i>=0;i--){
-            sell=max(sell,prices[i]);
-            cp=max(cp,sell-prices[i]);
-            mxi=max(cp,mxi);
-            profit[i]+=mxi;
-            ans=max(ans,profit[i]);
-        }
-        return ans;
+     // return solveRec(0,1,2,prices);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return solveMemo(0,1,2,prices,dp);
     }
 };
