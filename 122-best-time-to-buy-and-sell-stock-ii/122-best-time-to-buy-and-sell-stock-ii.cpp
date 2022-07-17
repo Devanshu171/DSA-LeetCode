eea4433870112e38cd85bf2dfd6bcb7dab5cf371
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 17:35:33 +0530

    Time: 21 ms (13.26%), Space: 15.3 MB (15.22%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -27,10 +27,32 @@ public:
         
        return dp[ind][buy]=profit;
     }
+    int solveTabu(vector<int>&prices){
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                 int profit=0;
+        
+        if(buy){
+            profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+        }else{
+            profit=max(prices[ind]+dp[ind+1][1],dp[ind+1][0]);
+        }
+        
+             dp[ind][buy]=profit;
+            }
+        }
+        
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
         // return solveRec(0,1,prices);
         int n=prices.size();
-        vector<vector<long long>>dp(n+1,vector<long long>(2,-1));
-        return solveMemo(0,1,prices,dp);
+        // vector<vector<long long>>dp(n+1,vector<long long>(2,-1));
+        // return solveMemo(0,1,prices,dp);
+        return solveTabu(prices);
     }
 };
