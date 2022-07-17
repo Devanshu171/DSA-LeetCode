69057f347af48183a742c073eb0e93e5dd4ecb18
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 21:01:03 +0530

    Time: 10 ms (37.96%), Space: 12.6 MB (8.24%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -33,24 +33,28 @@ public:
     }
     int solveTabu(vector<int>&prices){
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(2,0)));
         
         
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<2;buy++){
+                for(int cd=1;cd>=0;cd--){
                  int profit=0;
+                if(cd) profit= dp[ind+1][buy][0];
+
+                   else if(buy){
+                        profit=max(-prices[ind]+dp[ind+1][0][0],dp[ind+1][1][0]);
+                    }else{
+                        profit=max(prices[ind]+dp[ind+1][1][1],dp[ind+1][0][0]);
+                    }
         
-        if(buy){
-            profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
-        }else{
-            profit=max(prices[ind]+dp[ind+1][1],dp[ind+1][0]);
-        }
-        
-             dp[ind][buy]=profit;
+                     dp[ind][buy][cd]=profit;
+                }
             }
+                
         }
         
-        return dp[0][1];
+        return dp[0][1][0];
     }
      int solveTabuSo(vector<int>&prices){
         int n=prices.size();
@@ -76,10 +80,10 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         // return solveRec(0,1,0,prices);
-        int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
-        return solveMemo(0,1,0,prices,dp);
-        // return solveTabu(prices);
+        // int n=prices.size();
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
+        // return solveMemo(0,1,0,prices,dp);
+        return solveTabu(prices);
                 // return solveTabuSo(prices);
 
     }
