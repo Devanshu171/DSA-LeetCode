1a57bd9c5b9b59aaf7583a7c9bb380c617ff2f58
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 20:30:29 +0530

    Time: 17 ms (54.65%), Space: 10.9 MB (75.32%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+        int solveRec(int ind,int buy,int trans,vector<int>&prices){
+        if(ind==prices.size()) return 0;
+        if(trans==0) return 0;
+        
+        int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+solveRec(ind+1,0,trans,prices),solveRec(ind+1,1,trans,prices));
+        }else{
+             profit=max(prices[ind]+solveRec(ind+1,1,trans-1,prices),solveRec(ind+1,0,trans,prices));
+        }
+        return profit;
+    }
+     int solveMemo(int ind,int buy,int trans,vector<int>&prices,vector<vector<vector<int>>>&dp){
+        if(ind==prices.size()) return 0;
+        if(trans==0) return 0;
+        if(dp[ind][buy][trans]!=-1) return dp[ind][buy][trans];
+        int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+solveMemo(ind+1,0,trans,prices,dp),solveMemo(ind+1,1,trans,prices,dp));
+        }else{
+             profit=max(prices[ind]+solveMemo(ind+1,1,trans-1,prices,dp),solveMemo(ind+1,0,trans,prices,dp));
+        }
+        return dp[ind][buy][trans]= profit;
+    }
+    int solveTabu(int k ,vector<int>&prices){
+        int n=prices.size();
+                vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int trans=k;trans>=1;trans--){
+                      int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+dp[ind+1][0][trans],dp[ind+1][1][trans]);
+        }else{
+             profit=max(prices[ind]+dp[ind+1][1][trans-1],dp[ind+1][0][trans]);
+        }
+         dp[ind][buy][trans]= profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+
+    }
+      int solveTabuSo(int k,vector<int>&prices){
+        int n=prices.size();
+               vector<vector<int>>next(2,vector<int>(k+1,0));
+        vector<vector<int>>cur(2,vector<int>(k+1,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int trans=k;trans>=1;trans--){
+                      int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+next[0][trans],next[1][trans]);
+        }else{
+             profit=max(prices[ind]+next[1][trans-1],next[0][trans]);
+        }
+         cur[buy][trans]= profit;
+                }
+            }
+            next=cur;
+        }
+        return next[1][k];
+
+    }
+
+    int maxProfit(int k, vector<int>& prices) {
+            int n=prices.size();
+     // return solveRec(0,1,k,prices);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        // return solveMemo(0,1,k,prices,dp);
+        // return solveTabu(k,prices);
+        return solveTabuSo(k,prices);
+    }
+};
