520454a9f827163397b5b9ef07b7009edc5d94fc
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 17:15:01 +0530

    Time: 25 ms (8.10%), Space: 17.2 MB (7.62%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,10 +1,36 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int ans=0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]>prices[i-1]) ans+=prices[i]-prices[i-1];
+    int solveRec(int ind,bool buy,vector<int>&price){
+        if(ind==price.size()) return 0;
+        
+        if(buy){
+            int buy=-price[ind]+solveRec(ind+1,0,price);
+            int notBuy=solveRec(ind+1,1,price);
+            return max(buy,notBuy);
+        }else{
+            int sell=solveRec(ind+1,1,price)+price[ind];
+            int notSell=solveRec(ind+1,0,price);
+            return max(sell,notSell);
         }
-        return ans;
+    }
+    int solveMemo(int ind,int buy,vector<int>&price,vector<vector<long long>>&dp){
+      if(ind==price.size()) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        
+        int profit=0;
+        
+        if(buy){
+            profit=max(-price[ind]+solveMemo(ind+1,0,price,dp),solveMemo(ind+1,1,price,dp));
+        }else{
+            profit=max(price[ind]+solveMemo(ind+1,1,price,dp),solveMemo(ind+1,0,price,dp));
+        }
+        
+       return dp[ind][buy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        // return solveRec(0,1,prices);
+        int n=prices.size();
+        vector<vector<long long>>dp(n+1,vector<long long>(2,-1));
+        return solveMemo(0,1,prices,dp);
     }
 };
