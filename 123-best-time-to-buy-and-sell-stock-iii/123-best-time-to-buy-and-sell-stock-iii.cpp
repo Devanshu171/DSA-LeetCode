e3f6fef666fee8be8af1f5ee1b69a20ea663a2bc
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-07-17 19:40:36 +0530

    Time: 470 ms (40.93%), Space: 75.2 MB (85.13%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -26,6 +26,26 @@ public:
     }
     int solveTabu(vector<int>&prices){
         int n=prices.size();
+                vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int trans=2;trans>=1;trans--){
+                      int profit=0;
+        if(buy){
+             profit=max(-prices[ind]+dp[ind+1][0][trans],dp[ind+1][1][trans]);
+        }else{
+             profit=max(prices[ind]+dp[ind+1][1][trans-1],dp[ind+1][0][trans]);
+        }
+         dp[ind][buy][trans]= profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+
+    }
+      int solveTabuSo(vector<int>&prices){
+        int n=prices.size();
                vector<vector<int>>next(2,vector<int>(3,0));
         vector<vector<int>>cur(2,vector<int>(3,0));
         
@@ -51,6 +71,7 @@ public:
      // return solveRec(0,1,2,prices);
         // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
         // return solveMemo(0,1,2,prices,dp);
-        return solveTabu(prices);
+        // return solveTabu(prices);
+        return solveTabuSo(prices);
     }
 };
