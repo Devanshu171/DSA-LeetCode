87c48d5a32778438946ac13d08fe0cd67c963515
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-08-18 07:33:43 +0530

    Time: 203 ms (94.67%), Space: 78.4 MB (48.79%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,28 +2,43 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
       
-            int n=arr.size();
-        sort(begin(arr),end(arr));
-        vector<pair<int,int>>vec;
-        for(int i=0;i<n;i++){
-            int cnt=1;
-            while(i<n-1 && arr[i]==arr[i+1]){
-                cnt++;
-                i++;
-            }
-                vec.push_back({cnt,arr[i]});
+//             int n=arr.size();
+//         sort(begin(arr),end(arr));
+//         vector<pair<int,int>>vec;
+//         for(int i=0;i<n;i++){
+//             int cnt=1;
+//             while(i<n-1 && arr[i]==arr[i+1]){
+//                 cnt++;
+//                 i++;
+//             }
+//                 vec.push_back({cnt,arr[i]});
             
+//         }
+//         sort(begin(vec),end(vec));
+//         int ans=0;
+//         int sum=0;
+//         for(int i=vec.size()-1;i>=0;i--){
+//             ans++;
+//             cout<<vec[i].first<<endl;
+//             sum+=vec[i].first;
+//             if(sum>=n/2){
+//                 return ans;
+//             }
+//         }
+//         return ans;
+        unordered_map<int,int>mpp;
+        int n=arr.size();
+        priority_queue<int>pq;
+        for(auto it:arr) mpp[it]++;
+        for(auto it:mpp){
+            pq.push(it.second);
         }
-        sort(begin(vec),end(vec));
-        int ans=0;
-        int sum=0;
-        for(int i=vec.size()-1;i>=0;i--){
+        
+        int sum=0,ans=0;
+        while(sum<n/2){
             ans++;
-            cout<<vec[i].first<<endl;
-            sum+=vec[i].first;
-            if(sum>=n/2){
-                return ans;
-            }
+            sum+=pq.top();
+            pq.pop();
         }
         return ans;
         
