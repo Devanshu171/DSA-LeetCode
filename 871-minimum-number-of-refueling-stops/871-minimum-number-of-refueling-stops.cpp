12a2a5d01a4cde62fa9fefed4442079f5b966dff
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-08-20 12:37:04 +0530

    Time: 35 ms (78.31%), Space: 16.3 MB (51.25%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+     
+        
+       
+        int ans=0,prev=0;
+        int tank=startFuel;
+        int n=stations.size();
+        priority_queue<int>pq;
+        
+        for(int i=0;i<n;i++){
+            if(stations[i][0]>=target) break;
+            tank-=stations[i][0]-prev;
+            prev=stations[i][0];
+  
+            while(!pq.empty() && tank<0){
+                tank+=pq.top();
+                ans++;
+                pq.pop();
+            }
+                if(tank<0){
+                    return -1;
+                }
+            
+            pq.push(stations[i][1]);
+           
+        }
+        
+        tank-=target-prev;         
+            while(!pq.empty() && tank<0){
+                tank+=pq.top();
+                ans++;
+                pq.pop();
+            }
+                if(tank<0){
+                    return -1;
+                }
+        return ans;
+    }
+};
