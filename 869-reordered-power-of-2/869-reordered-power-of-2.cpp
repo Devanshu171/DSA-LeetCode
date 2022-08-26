efbe90ed6f5a2bea587c74b3e36284b592383e17
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-08-26 07:08:26 +0530

    Time: 30 ms (15.54%), Space: 6.1 MB (68.93%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        int count[10]={0};
+        
+        int m=n;
+        while(n){
+            count[n%10]++;
+            n/=10;
+        }
+        // cout<<pow(2,20)<<endl;
+        bool flag=true;
+        for(int i=0;i<31;i++){
+            int k=pow(2,i);
+            cout<<k<<endl;
+           vector<int>count2(10,0);
+            while(k){
+                count2[k%10]++;
+                k/=10;
+            }
+            for(int i=0;i<10;i++){
+                cout<<i<<" "<<count[i]<<" - "<<count2[i]<<endl;
+                if(count[i]!=count2[i]){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag) return true;
+            flag=true;
+        }
+        return false;
+    }
+};
