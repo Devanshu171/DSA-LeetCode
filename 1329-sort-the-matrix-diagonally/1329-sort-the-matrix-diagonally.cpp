5eec560d5b77567ee7edb26255a572c314757b84
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-08-28 19:13:40 +0530

    Time: 10 ms (90.91%), Space: 9.4 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -2,46 +2,60 @@ class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,0));
+//         vector<vector<int>>ans(n,vector<int>(m,0));
         
+//             for(int j=0;j<m;j++){
+//                 map<int,int>mpp;
+//                 int s=0,e=j;
+//                 int count=0;
+//                 while(s<n && e<m){
+//                     mpp[mat[s][e]]++;
+//                     count++;
+//                     s++;
+//                     e++;
+//                 }
+//                 s=0,e=j;
+//                 for(auto it:mpp){
+//                     while(it.second--){
+//                         mat[s++][e++]=it.first;
+                        
+//                     }
+//                 }
+//             }
+//         for(int i=1;i<n;i++){
+//              map<int,int>mpp;
+//                 int s=i,e=0;
+//                 int count=0;
+//                 while(s<n && e<m){
+//                     mpp[mat[s][e]]++;
+//                     count++;
+//                     s++;
+//                     e++;
+//                 }
+//                 s=i,e=0;
+//                 for(auto it:mpp){
+//                     while(it.second--){
+//                         mat[s++][e++]=it.first;
+                        
+//                     }
+//                 }
+//         }
+        
+//         return mat;
+        
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mpp;
+        
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                map<int,int>mpp;
-                int s=0,e=j;
-                int count=0;
-                while(s<n && e<m){
-                    mpp[mat[s][e]]++;
-                    count++;
-                    s++;
-                    e++;
-                }
-                s=0,e=j;
-                for(auto it:mpp){
-                    while(it.second--){
-                        mat[s++][e++]=it.first;
-                        
-                    }
-                }
+                mpp[i-j].push(mat[i][j]);
             }
-        for(int i=1;i<n;i++){
-             map<int,int>mpp;
-                int s=i,e=0;
-                int count=0;
-                while(s<n && e<m){
-                    mpp[mat[s][e]]++;
-                    count++;
-                    s++;
-                    e++;
-                }
-                s=i,e=0;
-                for(auto it:mpp){
-                    while(it.second--){
-                        mat[s++][e++]=it.first;
-                        
-                    }
-                }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mat[i][j]=mpp[i-j].top(); mpp[i-j].pop();
+            }
         }
         
         return mat;
-        
     }
 };
