097444ead175c6c5c52bdc3b1c8160731d0aac7c
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-10 09:30:31 +0530

    Time: 7 ms (54.82%), Space: 11.1 MB (29.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        
-        int start=0,end=nums.size()-1;
-        
-        while(start<=end){
-            
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target)
+    int bs(vector<int>nums,int s,int e,int target){
+        // int s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
                 return mid;
-            
-           else if(nums[start]<=nums[mid]){
-                if(target>=nums[start] && target<=nums[mid]){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
+            }else if(nums[mid]>target){
+                e=mid-1;
             }else{
-                if(target>=nums[mid] && target<=nums[end]){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
-                }
+                s=mid+1;
             }
-                
         }
         return -1;
+    }
+    int getPivot(vector<int>&nums){
+        int s=0,e=nums.size()-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]>=nums[0]){
+                s=mid+1;
+            }else{
+                e=mid;
+            }
+        }
+        return s;
+    }
+    int search(vector<int>& nums, int target) {
+        int pvt=getPivot(nums);
+        int fs=bs(nums,0,pvt-1,target);
+        int sc=bs(nums,pvt,nums.size()-1,target);
+        return fs==-1?sc:fs;
     }
 };
