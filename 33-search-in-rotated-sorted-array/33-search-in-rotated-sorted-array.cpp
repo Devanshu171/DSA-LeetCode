e8b7962dd70edae567245b9a76bbea0fead7555d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-10 09:45:35 +0530

    Time: 7 ms (54.82%), Space: 11.1 MB (29.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -27,9 +27,29 @@ public:
         return s;
     }
     int search(vector<int>& nums, int target) {
-        int pvt=getPivot(nums);
-        int fs=bs(nums,0,pvt-1,target);
-        int sc=bs(nums,pvt,nums.size()-1,target);
-        return fs==-1?sc:fs;
+        // int pvt=getPivot(nums);
+        // int fs=bs(nums,0,pvt-1,target);
+        // int sc=bs(nums,pvt,nums.size()-1,target);
+        // return fs==-1?sc:fs;
+        int s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[s]<=nums[mid]){
+                if(target>=nums[s] && target<=nums[mid]){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }else{
+                if(target>=nums[mid] && target<=nums[e]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }
+        }
+        return -1;
     }
 };
