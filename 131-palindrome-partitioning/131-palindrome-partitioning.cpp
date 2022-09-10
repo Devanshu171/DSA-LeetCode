5b89364db5e7efe0a0a520b88419373950b29587
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-10 19:00:12 +0530

    Time: 334 ms (19.97%), Space: 147 MB (14.22%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool checkPalin(string s){
+bool checkPalin(string s,int i,int j){
 	// cout<<s<<endl;
-	for(int i=0;i<s.size();i++){
-		if(s[i]!=s[s.size()-i-1]) return false;
+	while(i<=j){
+		if(s[i++]!=s[j--]) return false;
 	}
 	// cout<<s<<endl;
 	return true;
@@ -15,10 +15,10 @@ bool solve(int ind,string s,vector<string>temp,vector<vector<string>>&ans){
 		ans.push_back(temp);
 		return true;
 	}
-	for(int i=1;i<=s.size()-ind;i++){
-		if(checkPalin(s.substr(ind,i))){
-            temp.push_back(s.substr(ind,i));
-			if(solve(ind+i,s,temp,ans));
+	for(int i=ind;i<=s.size();i++){
+		if(checkPalin(s,ind,i)){
+            temp.push_back(s.substr(ind,i-ind+1));
+			if(solve(i+1,s,temp,ans));
             temp.pop_back();
 		}
 	}
