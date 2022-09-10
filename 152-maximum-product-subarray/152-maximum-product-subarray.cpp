f560906f4f306a727c570dd89b5e301d350996ff
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-10 11:50:02 +0530

    Time: 18 ms (20.10%), Space: 13.7 MB (94.89%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& arr) {
-      
-        int n=arr.size();
-        int p1=1,p2=1;
-	    int mx1=-10000+10,mx2=-10000+19;
-	    for(int i=0;i<n;i++){
-	        p1*=arr[i];
-	        p2*=arr[n-i-1];
-	        mx1=max(mx1,p1);
-	        mx2=max(mx2,p2);
-	        if(p1==0) p1=1;
-	        if (p2==0) p2=1;
-	    }
-	    return max(mx1,mx2);
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int curPrdtLeft=1,maxPrdtLeft=INT_MIN;
+        int curPrdtRight=1,maxPrdtRight=INT_MIN;
+        
+        
+        for(int i=0;i<n;i++){
+            curPrdtLeft*=nums[i];
+            curPrdtRight*=nums[n-i-1];
+            
+              maxPrdtLeft=max(maxPrdtLeft,curPrdtLeft);
+            maxPrdtRight=max(maxPrdtRight,curPrdtRight);
+            
+            if(curPrdtLeft==0) curPrdtLeft=1;
+            if(curPrdtRight==0) curPrdtRight=1;
+            
+          
+        }
+        return max(maxPrdtLeft,maxPrdtRight);
     }
 };
