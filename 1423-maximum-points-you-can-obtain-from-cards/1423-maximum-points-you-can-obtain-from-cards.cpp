0038c54084285ee6fc7c09f8984f425a0f497c67
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-11 23:51:15 +0530

    Time: 880 ms (5.00%), Space: 42.5 MB (46.88%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int maxScore(vector<int>& arr, int k) {
+    int maxScore(vector<int>& nums, int k) {
         
+        int n=nums.size();
+         k=n-k;
         
-        // need to find subarray of size n-k with min sum so that rest has max sum i.e score
-        int sum=0;
-        int n=arr.size();
-        for(int i=0;i<n;i++) sum+=arr[i];
-        if(k==n) return sum;
-        k=n-k;
-        int subsum=0;
-        int i=0,j=0;
-        int ans=INT_MAX;
+        int i=0,j=0,sum=0,tsum=0,ans=INT_MAX;
         while(j<n){
-            subsum+=arr[j];
+            tsum+=nums[j];
+            sum+=nums[j];
+             cout<<j-i+1<<" "<<k<<endl;
             if(j-i+1==k){
-                ans=min(ans,subsum);
-                subsum-=arr[i];
-                i++;
+               
+                ans=min(ans,sum);
+                sum-=nums[i++];
             }
             j++;
         }
-        
-        return sum-ans;
+        return tsum-(ans==INT_MAX?0:ans);
     }
 };
