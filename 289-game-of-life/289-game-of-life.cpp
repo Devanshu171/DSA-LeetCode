3848a0bd2c1f3d56c0c40b4eba6b1fa3872f3c6d
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-11 19:21:11 +0530

    Time: 4 ms (35.78%), Space: 7 MB (59.36%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -7,35 +7,35 @@ public:
         for(int i=0;i<8;i++){
             int newX=x+di[i],newY=y+dj[i];
             if(newX>=0 && newY>=0 && newX<board.size() && newY<board[0].size()){
-                ans+=board[newX][newY];
+                ans+=(board[newX][newY]==1 || board[newX][newY]==3);
             }
         }
         return ans;
     }
     void gameOfLife(vector<vector<int>>& board) {
         int n=board.size(),m=board[0].size();
-        vector<vector<int>>countn(n,vector<int>(m,0));
-        
+// 3->newly dead 2->newly dead
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int cnt=count(i,j,board);
-                countn[i][j]=cnt;
+                  if(board[i][j]==0){
+                    if(cnt==3) board[i][j]=2;
+                }else{
+                    if(cnt<2){
+                        board[i][j]=3;
+                    }else if(cnt==2 || cnt==3){
+                            continue;
+                    }else if(cnt>3){
+                        board[i][j]=3;
+                    }
+               }
             }
         }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]==0){
-                    if(countn[i][j]==3) board[i][j]=1;
-                }else{
-                    if(countn[i][j]<2){
-                        board[i][j]=0;
-                    }else if(countn[i][j]==2 || countn[i][j]==3){
-                            continue;
-                    }else if(countn[i][j]>3){
-                        board[i][j]=0;
-                    }
-               }
+              if(board[i][j]==3) board[i][j]=0;
+            else if(board[i][j]==2)board[i][j]=1;
             }
         }
     }
