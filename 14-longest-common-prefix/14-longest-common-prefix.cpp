ef76ba544462fc39937c2024d60d1d0fd5e2fb72
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-17 09:27:02 +0530

    Time: 10 ms (31.99%), Space: 9.2 MB (82.95%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,9 +4,10 @@ public:
         string ans=strs[0];
         for(int i=1;i<strs.size();i++){
             int j=0;
-            while(j<strs[i].size() && j<ans.size() && strs[i][j]==ans[j]) j++;
-            if(j==0)
+            while(j<ans.size() && j<strs[i].size() && ans[j]==strs[i][j]) j++;
+            if(j==0){
                 return "";
+            }
             ans=ans.substr(0,j);
         }
         return ans;
