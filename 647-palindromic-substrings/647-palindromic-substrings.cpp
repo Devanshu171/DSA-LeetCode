9b73ea58b9253ff5c7e89df194285db5743f0d70
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-17 11:07:44 +0530

    Time: 785 ms (10.89%), Space: 456.7 MB (5.44%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -10,19 +10,21 @@ public:
         return true;
     }
     
-    int countPalin(int ind,string &s){
+    int countPalin(int ind,string &s,vector<int>&dp){
         if(ind==s.size()) return 0;
-        
+        if(dp[ind]!=-1) return dp[ind];
         int cnt=0;
         
         for(int i=ind;i<s.size();i++){
             cnt+=isPalin(s.substr(ind,i-ind+1));
         }
 
-        return cnt+ countPalin(ind+1,s);
+        return dp[ind]= cnt+ countPalin(ind+1,s,dp);
     }
     int countSubstrings(string s) {
-        return countPalin(0,s);
+       
+        vector<int>dp(s.size(),-1);
+         return countPalin(0,s,dp);
         // cout<<s.substr(2,2)<<endl;
         // return 6;
         
