d4c06cd643b51b0998011b0d6ae8f5d038c93154
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-18 08:27:30 +0530

    Time: 8 ms (10.00%), Space: 6.5 MB (15.00%) - LeetHub

---
+++ 2409-count-days-spent-together/2409-count-days-spent-together.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
+        vector<int>month={0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int i=1;i<=12;i++) month[i]+=month[i-1];
+        int aad=month[stoi(arriveAlice.substr(0,2))-1]+stoi(arriveAlice.substr(3));
+        int ald=month[stoi(leaveAlice.substr(0,2))-1]+stoi(leaveAlice.substr(3));
+        int bad=month[stoi(arriveBob.substr(0,2))-1]+stoi(arriveBob.substr(3));
+        int bld=month[stoi(leaveBob.substr(0,2))-1]+stoi(leaveBob.substr(3));
+        unordered_map<int,int>mpp;
+        int ans=0;
+        for(int i=aad;i<=ald;i++) mpp[i]++;
+        for(int i=bad;i<=bld;i++){
+             mpp[i]++;
+            if(mpp[i]==2) ans++;
+        }
+        return ans;
+
+    }
+};
