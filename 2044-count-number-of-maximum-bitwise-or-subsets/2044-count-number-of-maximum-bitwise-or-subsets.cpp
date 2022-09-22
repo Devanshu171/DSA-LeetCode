ba5c56784bb8c82cdda17f1fdb9c9b669a140a21
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-09-22 07:15:08 +0530

    Time: 169 ms (31.10%), Space: 307.6 MB (5.46%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -8,9 +8,20 @@ public:
         int b=solve(ind+1,nums,target,Or);
         return a+b;
     }
+       int solveMemo(int ind,vector<int>&nums,int target,int Or,vector<vector<int>>&dp){
+        if(ind==nums.size()){
+            return Or==target;
+        }
+           if(dp[ind][Or]!=-1) return dp[ind][Or];
+        int a=solveMemo(ind+1,nums,target,Or|nums[ind],dp);
+        int b=solveMemo(ind+1,nums,target,Or,dp);
+        return dp[ind][Or]= a+b;
+    }
     int countMaxOrSubsets(vector<int>& nums) {
         int mOR=0;
         for(auto it:nums) mOR|=it;
-       return solve(0,nums,mOR,0);
+       // return solve(0,nums,mOR,0);
+        vector<vector<int>>dp(nums.size(),vector<int>(mOR+1,-1));
+        return solveMemo(0,nums,mOR,0,dp);
     }
 };
