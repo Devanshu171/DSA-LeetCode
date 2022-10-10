b47c57948049254b4a64140404e24dc4220acaac
Devanshu Sharma <91886198+Devanshu171@users.noreply.github.com>
2022-10-10 06:38:09 +0530

    Time: 3 ms (45.61%), Space: 6.2 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -10,19 +10,27 @@ public:
         return true;
     }
     string breakPalindrome(string s) {
-        string ans="";
-        for(int i=0;i<s.size();i++){
+//         string ans="";
+//         for(int i=0;i<s.size();i++){
             
-            for(char j='a';j<'z';j++){
-                string temp=s;
-                temp[i]=j;
-                if(!checkPalin(temp)){
-                    if(ans.size()==0 || ans>temp)
-                    ans=temp;
-                    break;
-                }
+//             for(char j='a';j<'z';j++){
+//                 string temp=s;
+//                 temp[i]=j;
+//                 if(!checkPalin(temp)){
+//                     if(ans.size()==0 || ans>temp)
+//                     ans=temp;
+//                     break;
+//                 }
+//             }
+//         }
+        for(int i=0;i<s.size()/2;i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                return s;
             }
         }
-        return ans;
+        s[s.size()-1]='b';
+        return s.size()<2?"":s;
+        // return ans;
     }
 };
